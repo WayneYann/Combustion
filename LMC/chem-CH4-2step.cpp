b3b852abd4b46dfd4ef6594f4c5d1b51f36dd2bf
marc <marc>
2006-11-30 17:53:06 +0000

    protect against negatives in other progressRate fn

--- LMC/chem-CH4-2step.cpp
+++ LMC/chem-CH4-2step.cpp
@@ -2055,21 +2055,42 @@ void progressRate(double * qdot, double * sc, double T)
     gibbs(g_RT, tc);
 
     /*reaction 1: 2 CH4 + 3 O2 => 2 CO + 4 H2O */
-    phi_f = sc[2]*sc[0];
+    if (sc[2]<0. || sc[0]<0)
+    {
+        phi_f = 0.;
+    }
+    else
+    {
+        phi_f = sc[2]*sc[0];
+    }
     k_f = 1e-6 * 1.545e+14*exp(0.5*tc[0]-20078/tc[1]);
     q_f = phi_f * k_f;
     q_r = 0.0;
     qdot[0] = q_f - q_r;
 
     /*reaction 2: 2 CO + O2 => 2 CO2 */
-    phi_f = sc[3]*pow(sc[1],0.5)*pow(sc[0],0.25);
+    if (sc[3]<0. || sc[0]<0 || sc[1]<0)
+    {
+        phi_f = 0.;
+    }
+    else
+    {
+        phi_f = sc[3]*pow(sc[1],0.5)*pow(sc[0],0.25);
+    }
     k_f = 3.16228e-5 * 1.991e+14*exp(-20130.9/tc[1]);
     q_f = phi_f * k_f;
     q_r = 0.0;
     qdot[1] = q_f - q_r;
 
     /*reaction 3: 2 CO2 => 2 CO + O2 */
-    phi_f = sc[4];
+    if (sc[4]<0.)
+    {
+        phi_f = 0.;
+    }
+    else
+    {
+        phi_f = sc[4];
+    }
     k_f = 2.5e+08*exp(-20130.9/tc[1]);
     q_f = phi_f * k_f;
     q_r = 0.0;
