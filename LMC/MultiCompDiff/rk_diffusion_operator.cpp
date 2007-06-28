b9c96ba3c8173a84f5fd298dc31afe7b183698ca
sepp <sepp>
2007-06-28 22:39:35 +0000

    1) reorder arguments

--- LMC/MultiCompDiff/rk_diffusion_operator.cpp
+++ LMC/MultiCompDiff/rk_diffusion_operator.cpp
@@ -7,7 +7,6 @@ HeatTransfer::rk_diffusion_operator (const Real time,
 {
     BL_PROFILE(BL_PROFILE_THIS_NAME() + "::rk_diffusion_operator()");
 
-
     // check that time is either prev_time or cur_time
     const TimeLevel whichTime = which_time(State_Type, time);
     BL_ASSERT(whichTime == AmrOldTime || whichTime == AmrNewTime);
@@ -67,26 +66,28 @@ HeatTransfer::rk_diffusion_operator (const Real time,
 	Array<int> bc = getBCArray (State_Type, idx, 0, ncomps);
 
 /*
+c     arguments are alphabetical, mostly:
+c
 c     lo, hi,                           ! INPUT limits of valid region
 c     areax, DIMS(areax),               ! INPUT areas of the faces perpendicular to x axis
 c     areay, DIMS(areay),               ! INPUT areas of the faces perpendicular to y axis
 c     bc,                               ! INPUT boundary condition array for all comps
 c     dx,                               ! INPUT physical dimensions of grid cells
-c     state, DIMS(state),               ! INPUT all variables in the state
-c     update_for_H, DIMS(update_for_H), ! OUTPUT divergences of the fluxes
-c     update_for_Y, DIMS(update_for_Y), ! OUTPUT divergences of the fluxes
-c     xflux_for_H, DIMS(xflux_for_H),   ! OUTPUT x fluxes for enthalpy
-c     yflux_for_H, DIMS(yflux_for_H),   ! OUTPUT y fluxes for enthalpy
-c     xflux_for_Y, DIMS(xflux_for_Y),   ! OUTPUT x fluxes for species
-c     yflux_for_Y, DIMS(yflux_for_Y),   ! OUTPUT y fluxes for species
-c     volume, DIMS(volume),             ! INPUT volumes of the cells
-c     ncomps,                           ! INPUT total number of components in the state
-c     nspecies,                         ! INPUT total number of species in the state
 c     index_of_firstY,                  ! INPUT index of rho Y for the first species in the state
 c     index_of_lastY,                   ! INPUT index of rho Y for the last species in the state
 c     index_of_rho,                     ! INPUT index of rho in the state
 c     index_of_rhoH,                    ! INPUT index of rho H in the state
 c     index_of_T                        ! INPUT index of T in the state
+c     ncomps,                           ! INPUT total number of components in the state
+c     nspecies,                         ! INPUT total number of species in the state
+c     state, DIMS(state),               ! INPUT all variables in the state
+c     update_for_H, DIMS(update_for_H), ! OUTPUT divergences of the fluxes
+c     update_for_Y, DIMS(update_for_Y), ! OUTPUT divergences of the fluxes
+c     volume, DIMS(volume),             ! INPUT volumes of the cells
+c     xflux_for_H, DIMS(xflux_for_H),   ! OUTPUT x fluxes for enthalpy
+c     xflux_for_Y, DIMS(xflux_for_Y),   ! OUTPUT x fluxes for species
+c     yflux_for_H, DIMS(yflux_for_H),   ! OUTPUT y fluxes for enthalpy
+c     yflux_for_Y, DIMS(yflux_for_Y),   ! OUTPUT y fluxes for species
 */
 
 #define DATA_AND_LIMITS(foo) foo.dataPtr(),foo.loVect()[0],foo.loVect()[1],foo.hiVect()[0],foo.hiVect()[1]
@@ -96,21 +97,21 @@ c     index_of_T                        ! INPUT index of T in the state
 			       DATA_AND_LIMITS(area[1][idx]),
 			       bc.dataPtr(),
 			       geom.CellSize(),
-			       DATA_AND_LIMITS(state[idx]),
-			       DATA_AND_LIMITS((*update_for_H)[update_for_H_mfi]),
-			       DATA_AND_LIMITS((*update_for_Y)[update_for_Y_mfi]),
-			       DATA_AND_LIMITS((*flux_for_H[0])[xflux_for_H_mfi]),
-			       DATA_AND_LIMITS((*flux_for_H[1])[yflux_for_H_mfi]),
-			       DATA_AND_LIMITS((*flux_for_Y[0])[xflux_for_Y_mfi]),
-			       DATA_AND_LIMITS((*flux_for_Y[1])[yflux_for_Y_mfi]),
-			       DATA_AND_LIMITS(volume[idx]),
-			       &ncomps,
-			       &nspecies,
 			       &index_of_firstY,
 			       &index_of_lastY,
 			       &index_of_rho,
 			       &index_of_rhoH,
-			       &index_of_T);
+			       &index_of_T,
+			       &ncomps,
+			       &nspecies,
+			       DATA_AND_LIMITS(state[idx]),
+			       DATA_AND_LIMITS((*update_for_H)[update_for_H_mfi]),
+			       DATA_AND_LIMITS((*update_for_Y)[update_for_Y_mfi]),
+			       DATA_AND_LIMITS(volume[idx]),
+			       DATA_AND_LIMITS((*flux_for_H[0])[xflux_for_H_mfi]),
+			       DATA_AND_LIMITS((*flux_for_Y[0])[xflux_for_Y_mfi]),
+			       DATA_AND_LIMITS((*flux_for_H[1])[yflux_for_H_mfi]),
+			       DATA_AND_LIMITS((*flux_for_Y[1])[yflux_for_Y_mfi]));
     }
 
     // stop here, more to come
