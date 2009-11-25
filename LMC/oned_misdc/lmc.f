13a27092eae41b4be6e165ffda844b7d07d28853
marc <marc>
2009-11-25 02:33:57 +0000

    *** empty log message ***

--- LMC/oned_misdc/lmc.f
+++ LMC/oned_misdc/lmc.f
@@ -105,7 +105,7 @@ c      write(*,fortin)
       
       dx = (probhi-problo)/DBLE(nx)
       nscal = Nspec+4
-
+      
       call probinit(problo,probhi)
       
       if ( chkfile .ne. 'null') then
@@ -116,7 +116,7 @@ c      write(*,fortin)
      $                   Ydot_new,divu_new,dsdt,intra,
      $                   time,at_nstep,dt_old,cfl_used)
          do_init = 0
-               
+            
          do i = -1,nx
             vel_old(i) =  vel_new(i)
             do n = 1,nscal
@@ -133,171 +133,174 @@ c      write(*,fortin)
             press_old(i) =  press_new(i)
          enddo
          
-c         call minmax_vel(nx,vel_new)
+c     call minmax_vel(nx,vel_new)
          
       else
+         
+         do_init = 1
+         time = 0.d0
+         at_nstep = 0
 
-          do_init = 1
-          time = 0.d0
-          at_nstep = 0
-
-          call initdata(nx,vel_new,scal_new,Ydot_new,dx)
-          call set_bc_grow_s(nx,scal_new,dx,time)
-          do i = -1,nx
+         call initdata(nx,vel_new,scal_new,Ydot_new,dx)
+         call set_bc_grow_s(nx,scal_new,dx,time)
+         do i = -1,nx
             do n = 1,nscal
-              scal_old(i,n) = scal_new(i,n)
+               scal_old(i,n) = scal_new(i,n)
             enddo
-          enddo
-
-          call minmax_vel(nx,vel_new)
-
-c         Define density for initial projection.
-          do i = -1,nx
+         enddo
+         
+         call minmax_vel(nx,vel_new)
+         
+c     Define density for initial projection.
+         do i = -1,nx
             rhohalf(i) = scal_old(i,Density)
-          enddo
-
+         enddo
+         
 c     FIXME: Currently computes beta on bc using grow vals (see above)
 c              Should rather use bc vals, and modify stencil for laplacians
-          call calc_diffusivities(nx,scal_new,beta_new,mu_new)
-
-          call calc_divu(nx,scal_new,beta_new,Ydot_new,divu_new,dx,time)
-
-          call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
+         call calc_diffusivities(nx,scal_new,beta_new,mu_new)
+         
+         call calc_divu(nx,scal_new,beta_new,Ydot_new,divu_new,dx,time)
+         
+         call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
      $                cfl,umax,dx,dt)
 
-          print *,'initialVelocityProject: '
-          dt_dummy = -1.d0
+         print *,'initialVelocityProject: '
+         dt_dummy = -1.d0
 
-          do i=0,nx-1
-             vel_old(i) = vel_new(i)
-          enddo
-          call project(nx,vel_old,vel_new,rhohalf,divu_new,
-     $                 press_old,press_new,dx,dt_dummy,time)
+         do i=0,nx-1
+            vel_old(i) = vel_new(i)
+         enddo
+         call project(nx,vel_old,vel_new,rhohalf,divu_new,
+     $                press_old,press_new,dx,dt_dummy,time)
 
-          call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
-     $                cfl,umax,dx,dt)
+         call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
+     $               cfl,umax,dx,dt)
 
-          dt = dt * init_shrink
+         dt = dt * init_shrink
 
-          dt_init = dt
+         dt_init = dt
 
-          do i = -1,nx
-             vel_old(i) =  vel_new(i)
-          enddo
-          
-          do i = 0,nx
-             do n = 1,nscal
-                scal_hold(i,n) = scal_old(i,n)
-             enddo
-          enddo
-          
-          do i = 0,nx-1
-             do n = 1,nscal
-                const_src(i,n) = 0.d0
-                lin_src_old(i,n) = 0.d0
-                lin_src_new(i,n) = 0.d0
-             enddo
-          enddo
-          
-          print *,' '
-          print *,'...doing num_divu_iters = ',num_divu_iters 
-          print *,' '
+         do i = -1,nx
+            vel_old(i) =  vel_new(i)
+         enddo
+         
+         do i = 0,nx
+            do n = 1,nscal
+               scal_hold(i,n) = scal_old(i,n)
+            enddo
+         enddo
+         
+         print *,'... set diff terms from initial data'
+         call get_spec_visc_terms(nx,scal_old,beta_old,
+     &                            lin_src_old(0,FirstSpec),dx,time)
+         call get_rhoh_visc_terms(nx,scal_old,beta_old,
+     &                            lin_src_old(0,RhoH),dx,time)
 
-          do nd = 1,num_divu_iters
-             print *,' ...doing divu_iter number',nd,' dt=',dt
+c     What do we do for aofs?
+         do i = 0,nx-1
+            do n = 1,nscal
+               const_src(i,n) = 0.d0
+               lin_src_new(i,n) = lin_src_old(i,n)
+            enddo
+         enddo
+         
+         print *,' '
+         print *,'...doing num_divu_iters = ',num_divu_iters 
+         print *,' '
+         
+         do nd = 1,num_divu_iters
 
-             call strang_chem(nx,scal_old,scal_new,
-     $                        const_src,lin_src_old,lin_src_new,
-     $                        intra,dt)
+            print *,' ...doing divu_iter number',nd,' dt=',dt
+            
+            call strang_chem(nx,scal_old,scal_new,
+     $                       const_src,lin_src_old,lin_src_new,
+     $                       intra,dt)
 
-             do i = 0,nx-1
-                do n = 1,nspec 
-                   is = FirstSpec-1+n
-                   Ydot_new(i,n) = 
-     $                  (scal_new(i,is)-scal_old(i,is))/dt
-                enddo
-                Ydot_new(i,0) = (scal_new(i,Temp)-scal_old(i,Temp))/dt
-             enddo
-             do i = 0,nx
-                do n = 1,nscal
-                   scal_new(i,n) = scal_hold(i,n)
-                enddo
-             enddo
+c     Extract Ydot, and reset state
+            do i = 0,nx-1
+               do n = 1,nspec 
+                  is = FirstSpec-1+n
+                  Ydot_new(i,n) = (scal_new(i,is)-scal_old(i,is))/dt
+               enddo
+               Ydot_new(i,0) = (scal_new(i,Temp)-scal_old(i,Temp))/dt
+               do n = 1,nscal
+                  scal_new(i,n) = scal_hold(i,n)
+               enddo
+            enddo
+            
+            call calc_divu(nx,scal_new,beta_new,Ydot_new,
+     &                     divu_new,dx,time)
 
-             call calc_divu(nx,scal_new,beta_new,Ydot_new,
-     &            divu_new,dx,time)
+            print *,'initialVelocityProject: '
+            dt_dummy = -1.d0
+            
+            call project(nx,vel_old,vel_new,rhohalf,divu_new,
+     $                   press_old,press_new,dx,dt_dummy,time)
 
-             print *,'initialVelocityProject: '
-             dt_dummy = -1.d0
+            print *,' '
+            print *,' '
+            dt_init = dt
+             
+            call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
+     $                  cfl,umax,dx,dt_init2)
+            dt_init2 = dt_init2 * init_shrink
+            dt = min(dt_init,dt_init2)
+            print *,' '
 
-             call project(nx,vel_old,vel_new,rhohalf,divu_new,
-     $            press_old,press_new,dx,dt_dummy,time)
-
-             print *,' '
-             print *,' '
-             dt_init = dt
-
-             call est_dt(nx,vel_new,scal_new,divu_new,dsdt,
-     $                   cfl,umax,dx,dt_init2)
-             dt_init2 = dt_init2 * init_shrink
-             dt = min(dt_init,dt_init2)
-             print *,' '
-          enddo
+            do i = 0,nx-1
+               do n = 0,Nspec
+                  Ydot_old(i,n) = Ydot_new(i,n)
+               enddo
+               vel_old(i) =  vel_new(i)
+               divu_old(i) = divu_new(i)
+            enddo
+         enddo
   
-          do i = 0,nx-1
-             do n = 0,nspec
-                Ydot_old(i,n) = Ydot_new(i,n)
-             enddo
-             vel_old(i) =  vel_new(i)
-             divu_old(i) = divu_new(i)
-          enddo
-          
-          do n = 1,num_init_iters
+         print *,' '
+         print *,'...doing num_divu_iters = ',num_divu_iters 
+         print *,' '
+         do n = 1,num_init_iters
 
-             call est_dt(nx,vel_new,scal_old,divu_old,dsdt,
-     $                   cfl,umax,dx,dt)
-             dt = dt * init_shrink
-             dt = min(dt,dt_init)
-             print *,' '
-             print *,'INITIAL PRESSURE ITERATION ',n
-             print *,' '
-             write(6,1001) time,dt
+            call est_dt(nx,vel_new,scal_old,divu_old,dsdt,
+     $                  cfl,umax,dx,dt)
+            dt = dt * init_shrink
+            dt = min(dt,dt_init)
+            print *,' '
+            print *,'INITIAL PRESSURE ITERATION ',n
+            print *,' '
+            write(6,1001) time,dt
 
-c     Here we zero out intra before each advance. FIXME: WHY?
-             do ns = 1,nscal
-                do i = 0,nx-1
-                   intra(i,n) = 0.d0
-                enddo
-             enddo
-             
-             call advance(nx,vel_old,vel_new,scal_old,scal_new,
-     $                    Ydot_old,Ydot_new,press_old,press_new,
-     $                    divu_old,divu_new,dsdt,beta_old,beta_new,
-     $                    intra,dx,dt,time)
-             call minmax_vel(nx,vel_new)
+            call advance(nx,vel_old,vel_new,scal_old,scal_new,
+     $                   Ydot_old,Ydot_new,press_old,press_new,
+     $                   divu_old,divu_new,dsdt,beta_old,beta_new,
+     $                   intra,dx,dt,time)
+            call minmax_vel(nx,vel_new)
 
-             do i = 0,nx-1
-                vel_new(i)  =   vel_old(i)
-             enddo
+            do i = 0,nx-1
+               vel_new(i)  =   vel_old(i)
+            enddo
 
-             do i = 0,nx-1
-                do ns = 1,nscal
-                   scal_new(i,ns) =  scal_hold(i,ns)
-                   scal_old(i,ns) =  scal_hold(i,ns)
-                enddo
-             enddo
-             do i = 0,nx-1
-                do ns = 0,nspec
-                   Ydot_new(i,ns) =  Ydot_old(i,ns)
-                enddo
-                divu_new(i) = divu_old(i)
-             enddo
-             
+c     Reset state, Ydot
+            do i = 0,nx-1
+               do ns = 1,nscal
+                  scal_new(i,ns) =  scal_hold(i,ns)
+                  scal_old(i,ns) =  scal_hold(i,ns)
+               enddo
+               do ns = 0,nspec
+                  Ydot_new(i,ns) =  Ydot_old(i,ns)
+               enddo
+               divu_new(i) = divu_old(i)
+            enddo
+            do i = 0,nx-1
+            enddo
+            
             do i = 0,nx
                press_old(i)  = press_new(i)
             enddo
             
          enddo
+         
  1001    format('Advancing: starting time = ',
      $        e15.9,' with dt = ',e15.9)
          
@@ -306,14 +309,6 @@ c     Here we zero out intra before each advance. FIXME: WHY?
          
          cfl_used = cfl * init_shrink
          
-c     Here we zero out intra before the "real" time stepping,
-c     but not within advance, so we use a lagged intra each time.
-c     FIXME: Why start on a bad note?
-         do n = 1,nscal
-            do i = 0,nx-1
-               intra(i,n) = 0.d0
-            enddo
-         enddo
       endif
       end
 
