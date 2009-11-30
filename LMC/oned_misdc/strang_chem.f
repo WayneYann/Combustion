6d0b0c3326a8d0d7b93743e0693e1a8d843f8156
marc <marc>
2009-11-30 13:54:01 +0000

    *** empty log message ***

--- LMC/oned_misdc/strang_chem.f
+++ LMC/oned_misdc/strang_chem.f
@@ -95,12 +95,11 @@ c     Define change in state due to chemistry.
          enddo
          rho_half = 0.5d0*(rho_old + scal_new(i,Density))
          CALL CKCPBS(0.5*(scal_old(i,temp)+Tnew),Yhalf,IWRK,RWRK,cp)
-c     NOTE: This is not right....
-         I_R(i,0) = (
-     $           (scal_new(i,RhoH)-scal_old(i,RhoH)) / dt
-     $           - const_src(i,RhoH)
-     $           - 0.5d0*(lin_src_old(i,RhoH)+lin_src_new(i,RhoH)) )/
-     $        (rho_half*cp)
+         I_R(i,0) = 
+     $           (scal_new(i,Temp)-scal_old(i,Temp)) / dt
+     $            - (    const_src(i,RhoH)
+     $           + 0.5d0*(lin_src_old(i,RhoH)+lin_src_new(i,RhoH)) )/
+     $                         (rho_half*cp)
       enddo
 
       print *,' '
