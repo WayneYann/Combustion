30aceb3e20f84175093d06a8bcd5d3bfe32584bd
gilet <gilet>
2010-01-06 21:46:48 +0000

    fixed a bug in rhoDgradHgradY()

--- LMC/oned_misdc/get_temp_visc_terms.f
+++ LMC/oned_misdc/get_temp_visc_terms.f
@@ -16,6 +16,15 @@ C CEG:: the first thing rhoDgradHgradY() does is set grow cells
 C      call set_bc_grow_s(scal,dx,time)
       call rhoDgradHgradY(scal,beta,visc,dx,time)
 
+      open(UNIT=11, FILE='rdghgy.dat', STATUS = 'REPLACE')
+      do i = 0,nx-1
+         write(11,1006) i,1.d-1*visc(i)
+      enddo
+ 1006 FORMAT((I5,1X),(E22.15,1X))    
+      stop       
+
+
+
 c     Add Div( lambda Grad(T) )
       dxsqinv = 1.d0/(dx*dx)
       do i = 0,nx-1
@@ -43,10 +52,14 @@ c     Add Div( lambda Grad(T) )
       
       integer i,n,is,IWRK
       real*8 beta_lo,beta_hi
-      real*8 flux_lo,flux_hi
+      real*8 rdgydgh_lo,rdgydgh_hi
       real*8 dxsqinv,RWRK,rho,dv
       real*8 hi(maxspec,-1:nx)
       real*8 Y(maxspec,-1:nx)
+
+C FIXME
+      real*8 beta_edge(-1:nx  ,Nspec)      
+      integer j
 
 c     Compute rhoD Grad(Yi).Grad(hi) terms
 
@@ -61,8 +74,22 @@ c     Get Hi, Yi at cell centers
          call CKHMS(scal(i,Temp),IWRK,RWRK,hi(1,i))
          do n=1,Nspec
             Y(n,i) = scal(i,FirstSpec+n-1)/rho
+C CEG:: changing tobe consistent with LMC
+C            Y(n,i) = scal(i,FirstSpec+n-1)/scal(i,Density)
          enddo
       enddo
+
+C debugging FIXME
+C$$$ 1007 FORMAT((I5,1X),6(E22.15,1X))      
+C$$$         open(UNIT=11, FILE='h.dat', STATUS = 'REPLACE')
+C$$$         write(11,*)'# 256 7' 
+C$$$         do j=0,nx-1
+C$$$            write(11,1007) j,(1.d-4*hi(n,j),n=1,Nspec)
+C$$$         enddo
+C$$$         close(11)
+C$$$         stop
+CCCCCCCCCCCCC
+
 
 c     Compute differences
       do i = 0,nx-1
@@ -77,13 +104,25 @@ c     Compute differences
                beta_hi = 0.5*(beta(i,is) + beta(i+1,is))
             endif
             
-            flux_lo = beta_lo*(Y(n,i)-Y(n,i-1))*(hi(n,i)-hi(n,i-1))
-            flux_hi = beta_hi*(Y(n,i+1)-Y(n,i))*(hi(n,i+1)-hi(n,i))
+C CEG:: debugging remove me!!
+            beta_edge(i,n) = beta_lo
+            beta_edge(i+1,n) = beta_hi
+CCCCC
+            rdgydgh_lo = beta_lo*(Y(n,i)-Y(n,i-1))*(hi(n,i)-hi(n,i-1))
+            rdgydgh_hi = beta_hi*(Y(n,i+1)-Y(n,i))*(hi(n,i+1)-hi(n,i))
             
-            dv = dv + flux_hi - flux_lo
+            dv = dv + (rdgydgh_hi + rdgydgh_lo)*0.5d0
          enddo
          
          visc(i) = dv*dxsqinv
         end do
+
+      open(UNIT=11, FILE='rdghgy.dat', STATUS = 'REPLACE')
+      write(11,*)
+      do i = 0,nx-1
+         write(11,1006) i,1.d-1*visc(i)
+      enddo
+ 1006 FORMAT((I5,1X),(E22.15,1X))    
+      stop       
 
       end
