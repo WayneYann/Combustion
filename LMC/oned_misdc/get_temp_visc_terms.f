a087fbd79fc87b428eca249318fc294c7442f386
ajnonaka <ajnonaka>
2011-05-14 00:39:01 +0000

    species diffusion fluxes are now conservative

--- LMC/oned_misdc/get_temp_visc_terms.f
+++ LMC/oned_misdc/get_temp_visc_terms.f
@@ -71,7 +71,12 @@ C       get_temp_visc_terms, so need this here
       real*8 hi(maxspec,-1:nx)
       real*8 Y(maxspec,-1:nx)
 
+      real*8 spec_flux_lo(0:nx-1,maxspec)
+      real*8 spec_flux_hi(0:nx-1,maxspec)
 
+      real*8 sum_lo,sum_hi
+      real*8 sumRhoY_lo,sumRhoY_hi
+      real*8 RhoYe_lo,RhoYe_hi
 
 CCCCCCCCCCC debugging FIXME
 C 1011 FORMAT((I5,1X),2(E22.15,1X))      
@@ -100,6 +105,10 @@ C      enddo
 c     Compute differences
       do i = 0,nx-1
          dv = 0.d0
+         sum_lo = 0.d0
+         sum_hi = 0.d0
+         sumRhoY_lo = 0.d0
+         sumRhoY_hi = 0.d0
          do n=1,Nspec
             is = FirstSpec + n - 1
             if (coef_avg_harm.eq.1) then
@@ -109,17 +118,49 @@ c     Compute differences
                beta_lo = 0.5d0*(beta(i,is) + beta(i-1,is))
                beta_hi = 0.5d0*(beta(i,is) + beta(i+1,is))
             endif
-            
-            rdgydgh_lo = beta_lo*(Y(n,i)-Y(n,i-1))*(hi(n,i)-hi(n,i-1))
-            rdgydgh_hi = beta_hi*(Y(n,i+1)-Y(n,i))*(hi(n,i+1)-hi(n,i))
 
-C$$$CCCCCCCCCCCCCCCCC            
-C$$$            write(11,1011) i,Y(n,i),Y(n,i-1)
-C$$$            write(11,1011) i,(Y(n,i)-Y(n,i-1)),(hi(n,i)-hi(n,i-1))
-C$$$            write(11,1011) i,hi(n,i),hi(n,i-1)
-C$$$            write(11,1011) i,scal(i,Temp),scal(i-1,Temp)
-C$$$            write(11,*)
-CCCCCCCCCCCCCCCCCCc
+            spec_flux_lo(i,n) = beta_lo*(Y(n,i)-Y(n,i-1))
+            spec_flux_hi(i,n) = beta_hi*(Y(n,i+1)-Y(n,i))
+
+            if (LeEQ1 .eq. 0) then
+
+c              need to correct fluxes so they add to zero on each face
+c              build up the sum of species fluxes on lo and hi faces
+c              this will be "rho * V_c"
+               sum_lo = sum_lo + spec_flux_lo(i,n)
+               sum_hi = sum_hi + spec_flux_hi(i,n)
+               
+c              build up the sum of rho*Y_m
+c              this will be the density
+               sumRhoY_lo = sumRhoY_lo+0.5d0*(scal(i-1,is)+scal(i,is))
+               sumRhoY_hi = sumRhoY_hi+0.5d0*(scal(i,is)+scal(i+1,is))
+               
+            end if
+
+         end do
+
+         if (LeEQ1 .eq. 0) then
+c           correct the fluxes so they add up to zero before computing visc
+            do n=1,Nspec
+               is = FirstSpec + n - 1
+
+c              compute rho*Y_m on each face
+               RhoYe_lo = .5d0*(scal(i-1,is)+scal(i,is))
+               RhoYe_hi = .5d0*(scal(i,is)+scal(i+1,is))
+
+c              set flux = flux - (rho*V_c)*(rho*Y_m)/rho
+               spec_flux_lo(i,n) = spec_flux_lo(i,n) 
+     $              - sum_lo*RhoYe_lo/sumRhoY_lo
+               spec_flux_hi(i,n) = spec_flux_hi(i,n) 
+     $              - sum_hi*RhoYe_hi/sumRhoY_hi
+
+            end do
+         end if
+
+         do n=1,Nspec
+            rdgydgh_lo = spec_flux_lo(i,n)*(hi(n,i)-hi(n,i-1))
+            rdgydgh_hi = spec_flux_hi(i,n)*(hi(n,i+1)-hi(n,i))
+
             dv = dv + (rdgydgh_hi + rdgydgh_lo)*0.5d0
          enddo
          
