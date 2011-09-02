cc58d343eb16d17ef9d466220711bdb81b05797c
ajnonaka <AJNonaka@lbl.gov>
2011-09-02 12:56:34 -0700

    updated convergence tests formatted for paper in tex

--- LMC/bin/NOT_WORKING_YET/oned_misdc/chemh_converge4.f90
+++ LMC/bin/NOT_WORKING_YET/oned_misdc/chemh_converge4.f90
@@ -1,4 +1,4 @@
-program gri_converge3
+program chemh_converge4
 
   implicit none
 
@@ -11,23 +11,23 @@ program gri_converge3
   integer nx_0,nx_1,nx_2,nx_3,rr_03,rr_13,rr_23
   real*8 time_0,time_1,time_2,time_3,sum
   
-  real*8 data0  (4096,114)
-  real*8 data1  (4096,114)
-  real*8 data2  (4096,114)
-  real*8 data3  (4096,114)
-  real*8 data3_0(4096,114)
-  real*8 data3_1(4096,114)
-  real*8 data3_2(4096,114)
+  real*8 data0  (4096,26)
+  real*8 data1  (4096,26)
+  real*8 data2  (4096,26)
+  real*8 data3  (4096,26)
+  real*8 data3_0(4096,26)
+  real*8 data3_1(4096,26)
+  real*8 data3_2(4096,26)
 
-  real*8 L0_03(114)
-  real*8 L1_03(114)
-  real*8 L2_03(114)
-  real*8 L0_13(114)
-  real*8 L1_13(114)
-  real*8 L2_13(114)
-  real*8 L0_23(114)
-  real*8 L1_23(114)
-  real*8 L2_23(114)
+  real*8 L0_03(26)
+  real*8 L1_03(26)
+  real*8 L2_03(26)
+  real*8 L0_13(26)
+  real*8 L1_13(26)
+  real*8 L2_13(26)
+  real*8 L0_23(26)
+  real*8 L1_23(26)
+  real*8 L2_23(26)
 
   read(*,*) in0
   read(*,*) in1
@@ -40,7 +40,7 @@ program gri_converge3
   read(99,*) time_0
 
   do i=0,nx_0-1
-     read(99,*) data0(i,1:114)
+     read(99,*) data0(i,1:26)
   end do
 
   open(10,file=in1,form='formatted')
@@ -49,7 +49,7 @@ program gri_converge3
   read(10,*) time_1
 
   do i=0,nx_1-1
-     read(10,*) data1(i,1:114)
+     read(10,*) data1(i,1:26)
   end do
 
   open(20,file=in2,form='formatted')
@@ -58,7 +58,7 @@ program gri_converge3
   read(20,*) time_2
 
   do i=0,nx_2-1
-     read(20,*) data2(i,1:114)
+     read(20,*) data2(i,1:26)
   end do
 
   open(30,file=in3,form='formatted')
@@ -67,7 +67,7 @@ program gri_converge3
   read(30,*) time_3
 
   do i=0,nx_3-1
-     read(30,*) data3(i,1:114)
+     read(30,*) data3(i,1:26)
   end do
 
   !!!!!!!!!!!!!!!!!!!
@@ -78,7 +78,7 @@ program gri_converge3
 
   !  coarsen fine data
   do i=0,nx_0-1
-     do j=1,114
+     do j=1,26
         sum = 0.d0
         do k=0,rr_03-1
            sum = sum + data3(rr_03*i+k,j)
@@ -92,7 +92,7 @@ program gri_converge3
   L2_03 = 0.d0
 
   do i=0,nx_0-1
-     do j=1,114
+     do j=1,26
         L0_03(j) = max(L0_03(j), abs(data0(i,j)-data3_0(i,j)))
         L1_03(j) = L1_03(j) + abs(data0(i,j)-data3_0(i,j))
         L2_03(j) = L2_03(j) + (data0(i,j)-data3_0(i,j))**2
@@ -109,7 +109,7 @@ program gri_converge3
 
   !  coarsen fine data
   do i=0,nx_1-1
-     do j=1,114
+     do j=1,26
         sum = 0.d0
         do k=0,rr_13-1
            sum = sum + data3(rr_13*i+k,j)
@@ -123,7 +123,7 @@ program gri_converge3
   L2_13 = 0.d0
 
   do i=0,nx_1-1
-     do j=1,114
+     do j=1,26
         L0_13(j) = max(L0_13(j), abs(data1(i,j)-data3_1(i,j)))
         L1_13(j) = L1_13(j) + abs(data1(i,j)-data3_1(i,j))
         L2_13(j) = L2_13(j) + (data1(i,j)-data3_1(i,j))**2
@@ -140,7 +140,7 @@ program gri_converge3
 
   !  coarsen fine data
   do i=0,nx_2-1
-     do j=1,114
+     do j=1,26
         sum = 0.d0
         do k=0,rr_23-1
            sum = sum + data3(rr_23*i+k,j)
@@ -154,7 +154,7 @@ program gri_converge3
   L2_23 = 0.d0
 
   do i=0,nx_2-1
-     do j=1,114
+     do j=1,26
         L0_23(j) = max(L0_23(j), abs(data2(i,j)-data3_1(i,j)))
         L1_23(j) = L1_23(j) + abs(data2(i,j)-data3_1(i,j))
         L2_23(j) = L2_23(j) + (data2(i,j)-data3_1(i,j))**2
@@ -170,23 +170,31 @@ program gri_converge3
   print*,"nx     =",nx_0,nx_1,nx_2,nx_3
   write(*,1000) "time   =",time_0,time_1,time_2,time_3
   print*,""
-  write(*,1001) "$Y(\rm O}_2)$        &",L1_03( 5)," & ",log(L1_03( 5)/L1_13( 5))/log(2.d0)," &",L1_13( 5)," & ", &
+  write(*,1001) "$Y(\rm H}_2)$          &",L1_03( 2)," & ",log(L1_03( 2)/L1_13( 2))/log(2.d0)," &",L1_13( 2)," & ", &
+       log(L1_13( 2)/L1_23( 2))/log(2.d0)," &",L1_23( 2)," \\"
+  write(*,1001) "$Y(\rm H})$            &",L1_03( 3)," & ",log(L1_03( 3)/L1_13( 3))/log(2.d0)," &",L1_13( 3)," & ", &
+       log(L1_13( 3)/L1_23( 3))/log(2.d0)," &",L1_23( 3)," \\"
+  write(*,1001) "$Y(\rm H})$            &",L1_03( 4)," & ",log(L1_03( 4)/L1_13( 4))/log(2.d0)," &",L1_13( 4)," & ", &
+       log(L1_13( 4)/L1_23( 4))/log(2.d0)," &",L1_23( 4)," \\"
+  write(*,1001) "$Y(\rm O}_2)$          &",L1_03( 5)," & ",log(L1_03( 5)/L1_13( 5))/log(2.d0)," &",L1_13( 5)," & ", &
        log(L1_13( 5)/L1_23( 5))/log(2.d0)," &",L1_23( 5)," \\"
-  write(*,1001) "$Y(\rm H}_2{\rm O})$ &",L1_03( 7)," & ",log(L1_03( 7)/L1_13( 7))/log(2.d0)," &",L1_13( 7)," & ", &
+  write(*,1001) "$Y(\rm OH})$           &",L1_03( 6)," & ",log(L1_03( 6)/L1_13( 6))/log(2.d0)," &",L1_13( 6)," & ", &
+       log(L1_13( 6)/L1_23( 6))/log(2.d0)," &",L1_23( 6)," \\"
+  write(*,1001) "$Y(\rm H}_2{\rm O})$   &",L1_03( 7)," & ",log(L1_03( 7)/L1_13( 7))/log(2.d0)," &",L1_13( 7)," & ", &
        log(L1_13( 7)/L1_23( 7))/log(2.d0)," &",L1_23( 7)," \\"
-  write(*,1001) "$Y(\rm CH}_4)$       &",L1_03(15)," & ",log(L1_03(15)/L1_13(15))/log(2.d0)," &",L1_13(15)," & ", &
-       log(L1_13(15)/L1_23(15))/log(2.d0)," &",L1_23(15)," \\"
-  write(*,1001) "$Y(\rm CO}_2)$       &",L1_03(17)," & ",log(L1_03(17)/L1_13(17))/log(2.d0)," &",L1_13(17)," & ", &
-       log(L1_13(17)/L1_23(17))/log(2.d0)," &",L1_23(17)," \\"
-  write(*,1001) "$Y(\rm N}_2)$        &",L1_03(49)," & ",log(L1_03(49)/L1_13(49))/log(2.d0)," &",L1_13(49)," & ", &
-       log(L1_13(49)/L1_23(49))/log(2.d0)," &",L1_23(49)," \\"
-  write(*,1001) "$\rho$               &",L1_03(55)," & ",log(L1_03(55)/L1_13(55))/log(2.d0)," &",L1_13(55)," & ", &
-       log(L1_13(55)/L1_23(55))/log(2.d0)," &",L1_23(55)," \\"
-  write(*,1001) "$T$                  &",L1_03(56)," & ",log(L1_03(56)/L1_13(56))/log(2.d0)," &",L1_13(56)," & ", &
-       log(L1_13(56)/L1_23(56))/log(2.d0)," &",L1_23(56)," \\"
-  write(*,1001) "$\rho h$             &",L1_03(57)," & ",log(L1_03(57)/L1_13(57))/log(2.d0)," &",L1_13(57)," & ", &
-       log(L1_13(57)/L1_23(57))/log(2.d0)," &",L1_23(57)," \\"
-  write(*,1001) "$U$                  &",L1_03(58)," & ",log(L1_03(58)/L1_13(58))/log(2.d0)," &",L1_13(58)," & ", &
-       log(L1_13(58)/L1_23(58))/log(2.d0)," &",L1_23(58)," \\"
+  write(*,1001) "$Y(\rm HO}_2)$         &",L1_03( 8)," & ",log(L1_03( 8)/L1_13( 8))/log(2.d0)," &",L1_13( 8)," & ", &
+       log(L1_13( 8)/L1_23( 8))/log(2.d0)," &",L1_23( 8)," \\"
+  write(*,1001) "$Y(\rm H}_2{\rm O}_2)$ &",L1_03( 9)," & ",log(L1_03( 9)/L1_13( 9))/log(2.d0)," &",L1_13( 9)," & ", &
+       log(L1_13( 9)/L1_23( 9))/log(2.d0)," &",L1_23( 9)," \\"
+  write(*,1001) "$Y(\rm N}_2)$          &",L1_03(10)," & ",log(L1_03(10)/L1_13(10))/log(2.d0)," &",L1_13(10)," & ", &
+       log(L1_13(10)/L1_23(10))/log(2.d0)," &",L1_23(10)," \\"
+  write(*,1001) "$\rho$                 &",L1_03(11)," & ",log(L1_03(11)/L1_13(11))/log(2.d0)," &",L1_13(11)," & ", &
+       log(L1_13(11)/L1_23(11))/log(2.d0)," &",L1_23(11)," \\"
+  write(*,1001) "$T$                    &",L1_03(12)," & ",log(L1_03(12)/L1_13(12))/log(2.d0)," &",L1_13(12)," & ", &
+       log(L1_13(12)/L1_23(12))/log(2.d0)," &",L1_23(12)," \\"
+  write(*,1001) "$\rho h$               &",L1_03(13)," & ",log(L1_03(13)/L1_13(13))/log(2.d0)," &",L1_13(13)," & ", &
+       log(L1_13(13)/L1_23(13))/log(2.d0)," &",L1_23(13)," \\"
+  write(*,1001) "$U$                    &",L1_03(14)," & ",log(L1_03(14)/L1_13(14))/log(2.d0)," &",L1_13(14)," & ", &
+       log(L1_13(14)/L1_23(14))/log(2.d0)," &",L1_23(14)," \\"
 
-end program gri_converge3
+end program chemh_converge4
