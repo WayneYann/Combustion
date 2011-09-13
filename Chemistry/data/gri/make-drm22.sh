7061c8096f2a0e3ffe6f6e68c85f07263975a820
Marc Day <MSDay@lbl.gov>
2011-09-12 17:44:54 -0700

    Update generation of gri-related cpp files to include new tran routines.

--- Chemistry/data/gri/make-drm22.sh
+++ Chemistry/data/gri/make-drm22.sh
@@ -1,11 +1,14 @@
-ln -sf drm22.dat drm22.mec
-ln -sf thermo12.dat drm22.therm
-ln -sf transport12.dat drm22.trans
-python ../../tools/fuego/Fuego/Pythia/products/bin/fmc.py -mechanism=drm22.mec -thermo=drm22.therm -name=mec.cpp
+CONVERT=../../tools/convert/convert.exe
+ln -fs drm22.dat drm22.mec
+ln -fs thermo12.dat drm22.therm 
+ln -fs transport12.dat drm22.trans
+python ../../tools/fuego/Pythia/products/bin/fmc.py -mechanism=drm22.mec -thermo=drm22.therm -name=mec.cpp
 echo Compiling drm22.cpp...
-cat mec.cpp ../header/header.start\
+${CONVERT} model_files_drm22.dat
+cat mec.cpp drm22-tran.cpp \
+            ../header/header.start\
             ../header/header.mec   drm22.mec\
             ../header/header.therm drm22.therm\
             ../header/header.trans drm22.trans\
             ../header/header.end > drm22.cpp
-rm -f mec.cpp
+rm -f mec.cpp drm22-tran.cpp
