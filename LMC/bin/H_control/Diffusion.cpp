0f38e50da6ddd25bbbabf03ae349915d903385ac
Marc Day <MSDay@lbl.gov>
2011-10-24 10:40:27 -0700

    Scaled all components of flux, rather than just the one

--- LMC/bin/H_control/Diffusion.cpp
+++ LMC/bin/H_control/Diffusion.cpp
@@ -373,7 +373,7 @@ Diffusion::diffuse_scalar (Real                   dt,
         visc_op->apply(Rhs,Soln);
         visc_op->compFlux(D_DECL(*fluxn[0],*fluxn[1],*fluxn[2]),Soln,LinOp::Inhomogeneous_BC,false,0,fluxComp);
         for (int i = 0; i < BL_SPACEDIM; ++i)
-            (*fluxn[i]).mult(-b/(dt*caller->Geom().CellSize()[i]));
+            (*fluxn[i]).mult(-b/(dt*caller->Geom().CellSize()[i]),fluxComp,1,0);
         delete visc_op;
     }
     else
@@ -743,7 +743,7 @@ Diffusion::diffuse_scalar (Real                   dt,
     bool do_applyBC = true;
     visc_op->compFlux(D_DECL(*fluxnp1[0],*fluxnp1[1],*fluxnp1[2]),Soln,LinOp::Inhomogeneous_BC,do_applyBC,0,fluxComp);
     for (int i = 0; i < BL_SPACEDIM; ++i)
-        (*fluxnp1[i]).mult(b/(dt*caller->Geom().CellSize()[i]));
+        (*fluxnp1[i]).mult(b/(dt*caller->Geom().CellSize()[i]),fluxComp,1,0);
     delete visc_op;
     //
     // Copy into state variable at new time, without bc's
@@ -1604,7 +1604,7 @@ Diffusion::diffuse_Ssync (MultiFab*              Ssync,
         bool do_applyBC = true;
         visc_op->compFlux(D_DECL(*flux[0],*flux[1],*flux[2]),Soln,LinOp::Inhomogeneous_BC,do_applyBC,0,fluxComp);
         for (int i = 0; i < BL_SPACEDIM; ++i)
-            (*flux[i]).mult(b/(dt*caller->Geom().CellSize()[i]),0);
+            (*flux[i]).mult(b/(dt*caller->Geom().CellSize()[i]),fluxComp,1,0);
     }
 
     MultiFab::Copy(*Ssync,Soln,0,sigma,1,0);
