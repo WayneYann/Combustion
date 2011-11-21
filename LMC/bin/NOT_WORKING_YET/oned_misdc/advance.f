6ed7cadb0eeea1cf7075b14d9251558abe62b4c6
ajnonaka <AJNonaka@lbl.gov>
2011-11-21 07:59:27 -0800

    compilation error fixed - oops

--- LMC/bin/NOT_WORKING_YET/oned_misdc/advance.f
+++ LMC/bin/NOT_WORKING_YET/oned_misdc/advance.f
@@ -316,14 +316,15 @@ c     If .true., use I_R in predictor is instantaneous value at t^n
 c     If .false., use I_R^lagged = I_R^kmax from previous time step
       if (.false.) then
          do i=0,nx-1
-         do n=1,Nspec
-            C(n) = scal_old(i,FirstSpec+n-1)*invmwt(n)
+            do n=1,Nspec
+               C(n) = scal_old(i,FirstSpec+n-1)*invmwt(n)
+            end do
+            call CKWC(scal_old(i,Temp),C,IWRK,RWRK,WDOTK)
+            do n=1,Nspec
+               I_R_new(i,n) = WDOTK(n)*mwt(n)/thickFacCH
+            end do
          end do
-         call CKWC(scal_old(i,Temp),C,IWRK,RWRK,WDOTK)
-         do n=1,Nspec
-            I_R_new(i,n) = WDOTK(n)*mwt(n)/thickFacCH
-         end do
-      end do
+      end if
 
 c     compute advective forcing term
       print *,'... computing advective forcing term = D^n + I_R^kmax'
