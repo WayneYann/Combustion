95f70667bfe0691600f223b8c64abaec64d06113
Marc Day <MSDay@lbl.gov>
2011-12-06 10:37:34 -0800

    convert LiDryer mech to include translib routines

--- Chemistry/data/LiDryer/make-LiDryer.sh
+++ Chemistry/data/LiDryer/make-LiDryer.sh
@@ -1,8 +1,11 @@
-python ../../tools/fuego/Fuego/Pythia/products/bin/fmc.py -mechanism=LiDryer.mec -thermo=LiDryer.therm -name=mec.cpp
+CONVERT=../../tools/convert/convert.exe
+python ../../tools/fuego/Pythia/products/bin/fmc.py -mechanism=LiDryer.mec -thermo=LiDryer.therm -name=mec.cpp
 echo Compiling LiDryer.cpp...
-cat mec.cpp ../header/header.start\
+${CONVERT} model_files_LiDryer.dat
+cat mec.cpp LiDryer-tran.cpp \
+            ../header/header.start\
             ../header/header.mec   LiDryer.mec\
             ../header/header.therm LiDryer.therm\
             ../header/header.trans LiDryer.trans\
             ../header/header.end > LiDryer.cpp
-rm -f mec.cpp
+rm -f mec.cpp LiDryer-tran.cpp
