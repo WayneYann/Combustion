02bc252dd16e0d5f32b29751612b69580dc4c081
Marc Day <MSDay@lbl.gov>
2012-01-31 09:59:01 -0800

    Add Cheng folder under LMC, a few data files under Chemistry, and various other bits

--- Chemistry/data/glar/make-glarSkel.sh
+++ Chemistry/data/glar/make-glarSkel.sh
@@ -1,10 +1,11 @@
 ln -fs glarTherm.dat glarSkel.therm
 ln -fs tranGlar.dat glarSkel.trans
-python ../../tools/fuego/Fuego/Pythia/products/bin/fmc.py -mechanism=glarSkel.mec -thermo=glarSkel.therm -name=mec.cpp
-echo Compiling glarSkel.cpp...
-cat mec.cpp ../header/header.start\
-            ../header/header.mec   glarSkel.mec\
-            ../header/header.therm glarSkel.therm\
-            ../header/header.trans glarSkel.trans\
+python ../../tools/fuego/Pythia/products/bin/fmc.py -mechanism=glarSkel.mec -thermo=glarSkel.therm -name=mec.cpp
+${CONVERT} model_files_glarSkep.mec
+eacaaho Compiling glarSkel.cpp...
+cat mec.cpp ../header/headeraa.start\
+            ../header/headeraa.mec   glarSkel.mec\
+            ../header/headeraa.therm glarSkel.therm\
+            ../header/headeraa.trans glarSkel.trans\
             ../header/header.end > glarSkel.cpp
 rm -f mec.cpp
