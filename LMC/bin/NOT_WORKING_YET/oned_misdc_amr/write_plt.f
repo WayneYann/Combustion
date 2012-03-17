2c01b34e6954ed9a6e9d9994411ecaac0298b345
ajnonaka <AJNonaka@lbl.gov>
2012-03-16 20:09:01 -0700

    index bug

--- LMC/bin/NOT_WORKING_YET/oned_misdc_amr/write_plt.f
+++ LMC/bin/NOT_WORKING_YET/oned_misdc_amr/write_plt.f
@@ -3,11 +3,11 @@
       include 'spec.h'
 
       integer nsteps
-      real*8   vel(0:nlevs-1,-2:nx+1)
-      real*8  scal(0:nlevs-1,-2:nx+1,nscal)
-      real*8 press(0:nlevs-1,-1:nx+1)
-      real*8  divu(0:nlevs-1, 0:nx-1)
-      real*8   I_R(0:nlevs-1,-1:nx  ,0:Nspec)
+      real*8   vel(0:nlevs-1,-2:nfine+1)
+      real*8  scal(0:nlevs-1,-2:nfine+1,nscal)
+      real*8 press(0:nlevs-1,-1:nfine+1)
+      real*8  divu(0:nlevs-1, 0:nfine-1)
+      real*8   I_R(0:nlevs-1,-1:nfine  ,0:Nspec)
       real*8    dx(0:nlevs-1)
       real*8 time
       integer lo(0:nlevs-1)
