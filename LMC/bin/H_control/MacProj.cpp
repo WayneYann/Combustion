91e8189f8edaa3499c09503fbefeaffab13f8481
ajnonaka <AJNonaka@lbl.gov>
2012-05-08 13:48:00 -0700

    properly pass density into force computation functions

--- LMC/bin/H_control/MacProj.cpp
+++ LMC/bin/H_control/MacProj.cpp
@@ -806,9 +806,9 @@ MacProj::mac_sync_compute (int                   level,
         //
         // Compute total forcing terms.
         //
-        godunov->Sum_tf_gp_visc(tforces, 0, vel_visc_terms[S_fpi], 0, Gp[i], 0, S, Density);
+        godunov->Sum_tf_gp_visc(tforces, 0, vel_visc_terms[S_fpi], 0, Gp[i], 0, Rho, 1);
         godunov->Sum_tf_divu_visc(S, BL_SPACEDIM, tforces, BL_SPACEDIM, numscal,
-                                  scal_visc_terms[S_fpi], 0, divu, 0, S, Density, 1);
+                                  scal_visc_terms[S_fpi], 0, divu, 0, Rho, 1, 1);
 
         if (use_forces_in_trans)
         {
@@ -830,7 +830,7 @@ MacProj::mac_sync_compute (int                   level,
         //
         D_TERM(bndry[0] = ns_level.getBCArray(State_Type,i,0,1);,
                bndry[1] = ns_level.getBCArray(State_Type,i,1,1);,
-               bndry[2] = ns_level.getBCArray(State_Type,i,2,1););
+               bndry[2] = ns_level.getBCArray(State_Type,i,2,1);)
 
         godunov->Setup(grids[i], dx, dt, 0,
                        D_DECL(xflux,yflux,zflux),
