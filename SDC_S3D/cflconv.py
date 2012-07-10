fbcba28d1dded64465a3a403eb466a56a1f99755
Matthew Emmett <memmett@unc.edu>
2012-07-10 16:14:02 -0400

    SDC_S3D: Various updates to scripts.

--- SDC_S3D/cflconv.py
+++ SDC_S3D/cflconv.py
@@ -9,7 +9,7 @@ from runutils import autorun
 
 #### config
 
-run = False
+run = True
 
 probin = {
   'tfinal':  1.0e-6, 
@@ -18,6 +18,7 @@ probin = {
 sdc = {
   'qtype': '"Gauss-Lobatto"',
   'tol_residual': -1.0,
+  'iters': 0,
   }
 
 runs = [ ('050', 0.5),
@@ -37,21 +38,21 @@ if run:
 
     for name, cfl in runs:
 
-        autorun('cflconv/rk3_cfl%s' % name,
-                probin, { 'cfl': cfl, 'method': 1 },
-                sdc,    {})
+        # autorun('cflconv/rk3_cfl%s' % name,
+        #         probin, { 'cfl': cfl, 'method': 1 },
+        #         sdc,    {})
 
-        autorun('cflconv/sdc3_cfl%s' % name, 
-                probin, { 'cfl': cfl, 'method': 2 },
-                sdc,    { 'nnodes': 3 })
+        # autorun('cflconv/sdc3_cfl%s' % name, 
+        #         probin, { 'cfl': cfl, 'method': 2 },
+        #         sdc,    { 'nnodes': 3 })
 
-        autorun('cflconv/sdc5_cfl%s' % name, 
-                probin, { 'cfl': cfl, 'method': 2 },
-                sdc,    { 'nnodes': 5 })
+        # autorun('cflconv/sdc5_cfl%s' % name, 
+        #         probin, { 'cfl': cfl, 'method': 2 },
+        #         sdc,    { 'nnodes': 5, 'tol_residual': 1e-4, 'iters': 100 })
 
         autorun('cflconv/sdc9_cfl%s' % name, 
                 probin, { 'cfl': cfl, 'method': 2 },
-                sdc,    { 'nnodes': 9, 'tol_residual': 1e-8 })
+                sdc,    { 'nnodes': 9, 'tol_residual': 1e-4, 'iters': 100 })
 
 
 #### compute errors
@@ -97,7 +98,7 @@ plt.figure()
 plt.loglog(cfl, rho_errors['rk3'], '-ok', label='RK3')
 plt.loglog(cfl, rho_errors['sdc3'], '-sb', label='SDC3')
 plt.loglog(cfl, rho_errors['sdc5'], '-^r', label='SDC5')
-plt.loglog(cfl, rho_errors['sdc9'], '-^r', label='SDC9')
+plt.loglog(cfl, rho_errors['sdc9'], '-vm', label='SDC9')
 plt.legend(loc='best')
 plt.xlabel('cfl')
 plt.ylabel('max abs error')
@@ -107,30 +108,30 @@ plt.figure()
 plt.loglog(cfl, energy_errors['rk3'], '-ok', label='RK3')
 plt.loglog(cfl, energy_errors['sdc3'], '-sb', label='SDC3')
 plt.loglog(cfl, energy_errors['sdc5'], '-^r', label='SDC5')
-plt.loglog(cfl, energy_errors['sdc9'], '-^r', label='SDC9')
+plt.loglog(cfl, energy_errors['sdc9'], '-vm', label='SDC9')
 plt.xlabel('cfl')
 plt.ylabel('max abs error')
 plt.title('energy')
 
-# e0 = energy_errors['rk3'][0]
-# x = [ cfl[0], cfl[-1] ]
-# y = [ e0, e0*(x[1]/x[0])**3 ]
-# plt.loglog(x, y, '--k', label='3rd order')
+e0 = energy_errors['rk3'][0]
+x = [ cfl[0], cfl[-1] ]
+y = [ e0, e0*(x[1]/x[0])**3 ]
+plt.loglog(x, y, '--k', label='3rd order')
 
-# e0 = energy_errors['sdc3'][0]
-# x = [ cfl[0], cfl[-1] ]
-# y = [ e0, e0*(x[1]/x[0])**4 ]
-# plt.loglog(x, y, '--b', label='4th order')
+e0 = energy_errors['sdc3'][0]
+x = [ cfl[0], cfl[-1] ]
+y = [ e0, e0*(x[1]/x[0])**4 ]
+plt.loglog(x, y, '--b', label='4th order')
 
-# e0 = energy_errors['sdc5'][0]
-# x = [ cfl[0], cfl[-1] ]
-# y = [ e0, e0*(x[1]/x[0])**8 ]
-# plt.loglog(x, y, '--r', label='8th order')
+e0 = energy_errors['sdc5'][0]
+x = [ cfl[0], cfl[-1] ]
+y = [ e0, e0*(x[1]/x[0])**8 ]
+plt.loglog(x, y, '--r', label='8th order')
 
-# e0 = energy_errors['sdc9'][0]
-# x = [ cfl[0], cfl[-1] ]
-# y = [ e0, e0*(x[1]/x[0])**8 ]
-# plt.loglog(x, y, '--r', label='16th order')
+e0 = energy_errors['sdc9'][0]
+x = [ cfl[0], cfl[-1] ]
+y = [ e0, e0*(x[1]/x[0])**8 ]
+plt.loglog(x, y, '--m', label='16th order')
 
 plt.legend(loc='best')
 
