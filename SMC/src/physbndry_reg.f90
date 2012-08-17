bc435064536fd35820badf2f785d1fa5dc104788
Weiqun Zhang <weiqunzhang@lbl.gov>
2012-08-16 21:13:15 -0700

    SMC: fine tune physbndry_reg

--- SMC/src/physbndry_reg.f90
+++ SMC/src/physbndry_reg.f90
@@ -9,12 +9,13 @@ module physbndry_reg_module
      integer :: idim = -1
      integer :: iface = -1
      integer :: nc = -1
+     logical, pointer :: globalstatus(:), localstatus(:)
      type(multifab) :: data
      type(layout) :: la
   end type physbndry_reg
 
   interface isValid
-     module procedure validFab
+     module procedure get_localstatus
   end interface
 
   private
@@ -45,6 +46,9 @@ contains
     pd = get_pd(la0)
     pmask = get_pmask(la0)
 
+    allocate(pbr%globalstatus(nb))
+    allocate(pbr%localstatus(nb))
+
     plo = lwb(pd)
     phi = upb(pd)
 
@@ -57,24 +61,34 @@ contains
 
        if (pmask(idim_in)) then
           hi = lo
+          pbr%globalstatus(ibox) = .false.
        else if (iface_in .eq. 0) then
           if (lo(idim_in) .ne. plo(idim_in)) then ! interior
              hi = lo
+             pbr%globalstatus(ibox) = .false.
           else
              hi(idim_in) = lo(idim_in)
+             pbr%globalstatus(ibox) = .true.
           end if
        else if (iface_in .eq. 1) then
           if (hi(idim_in) .ne. phi(idim_in)) then ! interior
              lo = hi
+             pbr%globalstatus(ibox) = .false.
           else
              lo(idim_in) = hi(idim_in)
+             pbr%globalstatus(ibox) = .true.
           end if
        else
           call bl_error("physbndry_reg_build: invalid iface")
        end if
        
        call build(bxs(ibox), lo, hi)
-       
+
+       if (remote(la0,ibox)) then
+          pbr%localstatus(ibox) = .false.
+       else
+          pbr%localstatus(ibox) = pbr%globalstatus(ibox)
+       end if
     end do
 
     call build(baa, bxs, sort=.false.)
@@ -94,24 +108,14 @@ contains
     pbr%nc = -1
     call destroy(pbr%la)
     call destroy(pbr%data)
-
+    deallocate(pbr%globalstatus, pbr%localstatus)
   end subroutine physbndry_reg_destroy
 
-  function validFab(pbr, ibox) result(r)
+  function get_localstatus(pbr, ibox) result(r)
     integer, intent(in) :: ibox
     type(physbndry_reg), intent(in) :: pbr
     logical :: r
-    type(box) :: bx
-    if (remote(pbr%data,ibox)) then
-       r = .false.
-    else
-       bx = get_box(pbr%data,ibox)
-       if (box_volume(bx) .eq. 1) then
-          r = .false.
-       else
-          r = .true.
-       end if
-    end if
-  end function validFab
+    r = pbr%localstatus(ibox)
+  end function get_localstatus
 
 end module physbndry_reg_module
