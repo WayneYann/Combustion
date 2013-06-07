29c2de0bdea19186360325624f5697fb0ae8abdc
ajnonaka <AJNonaka@lbl.gov>
2013-06-07 14:03:17 -0700

    get_temp_visc_terms now computes Gamma dot grad h in terms of X, not y

--- LMC/bin/1D_MISDC_WBAR/get_temp_visc_terms.f
+++ LMC/bin/1D_MISDC_WBAR/get_temp_visc_terms.f
@@ -7,7 +7,9 @@
       real*8 dx
       integer lo,hi
 
-c     Compute Div(lambda.Grad(T)) + rho.D.Grad(Hi).Grad(Yi)
+c     Compute Div(lambda.Grad(T)) + Gamma_i.Grad(Hi)
+
+c     compute Gamma_i.Grad(Hi)
       call gamma_dot_gradh(scal,beta,visc,dx,lo,hi)
 
 c     Add Div( lambda Grad(T) )
@@ -66,15 +68,17 @@ c     Add Div( lambda Grad(T) )
       real*8 gamma_lo(0:nfine-1,Nspec)
       real*8 gamma_hi(0:nfine-1,Nspec)
 
-      real*8 sum_lo,sum_hi
-      real*8 sumRhoY_lo,sumRhoY_hi
-      real*8 RhoYe_lo,RhoYe_hi
+      real*8 sum_gamma_lo,sum_gamma_hi
+      real*8 sumRhoX_lo,sumRhoX_hi
+      real*8 RhoXe_lo,RhoXe_hi
+      real*8 X(Nspec,-1:nfine)
+      real*8 scal_X(-2:nfine+1,nscal)
 
-c     Compute rhoD Grad(Yi).Grad(hi) terms
+c     Compute Gamma.Grad(hi) terms
 
       dxsqinv = 1.d0/(dx*dx)
 
-c     Get Hi, Yi at cell centers
+c     Get Hi, Yi, Xi, rho*Xi at cell centers
       do i=lo-1,hi+1
          rho = 0.d0
          do n=1,Nspec
@@ -84,15 +88,24 @@ c     Get Hi, Yi at cell centers
          do n=1,Nspec
             Y(n,i) = scal(i,FirstSpec+n-1)/rho
          enddo
+
+c     convert Y to X
+         CALL CKYTX(Y(:,i),IWRK,RWRK,X(:,i))
+
+c     compute rho*X
+         do n=1,Nspec
+            scal_X(i,FirstSpec+n-1) = scal(i,Density)*X(n,i)
+         end do
+
       enddo
 
 c     Compute differences
       do i=lo,hi
          dv = 0.d0
-         sum_lo = 0.d0
-         sum_hi = 0.d0
-         sumRhoY_lo = 0.d0
-         sumRhoY_hi = 0.d0
+         sum_gamma_lo = 0.d0
+         sum_gamma_hi = 0.d0
+         sumRhoX_lo = 0.d0
+         sumRhoX_hi = 0.d0
          do n=1,Nspec
             is = FirstSpec + n - 1
             if (coef_avg_harm.eq.1) then
@@ -103,21 +116,21 @@ c     Compute differences
                beta_hi = 0.5d0*(beta(i,is) + beta(i+1,is))
             endif
 
-            gamma_lo(i,n) = beta_lo*(Y(n,i)-Y(n,i-1))
-            gamma_hi(i,n) = beta_hi*(Y(n,i+1)-Y(n,i))
+            gamma_lo(i,n) = beta_lo*(X(n,i)-X(n,i-1))
+            gamma_hi(i,n) = beta_hi*(X(n,i+1)-X(n,i))
 
             if (LeEQ1 .eq. 0) then
 
 c              need to correct fluxes so they add to zero on each face
 c              build up the sum of species fluxes on lo and hi faces
 c              this will be "rho * V_c"
-               sum_lo = sum_lo + gamma_lo(i,n)
-               sum_hi = sum_hi + gamma_hi(i,n)
+               sum_gamma_lo = sum_gamma_lo + gamma_lo(i,n)
+               sum_gamma_hi = sum_gamma_hi + gamma_hi(i,n)
                
-c              build up the sum of rho*Y_m
+c              build up the sum of rho*X_m
 c              this will be the density
-               sumRhoY_lo = sumRhoY_lo+0.5d0*(scal(i-1,is)+scal(i,is))
-               sumRhoY_hi = sumRhoY_hi+0.5d0*(scal(i,is)+scal(i+1,is))
+               sumRhoX_lo = sumRhoX_lo+0.5d0*(scal_X(i-1,is)+scal_X(i,is))
+               sumRhoX_hi = sumRhoX_hi+0.5d0*(scal_X(i,is)+scal_X(i+1,is))
                
             end if
 
@@ -128,15 +141,15 @@ c           correct the fluxes so they add up to zero before computing visc
             do n=1,Nspec
                is = FirstSpec + n - 1
 
-c              compute rho*Y_m on each face
-               RhoYe_lo = .5d0*(scal(i-1,is)+scal(i,is))
-               RhoYe_hi = .5d0*(scal(i,is)+scal(i+1,is))
+c              compute rho*X_m on each face
+               RhoXe_lo = .5d0*(scal_X(i-1,is)+scal_X(i,is))
+               RhoXe_hi = .5d0*(scal_X(i,is)+scal_X(i+1,is))
 
-c              set flux = flux - (rho*V_c)*(rho*Y_m)/rho
+c              set flux = flux - (rho*V_c)*(rho*X_m)/rho
                gamma_lo(i,n) = gamma_lo(i,n) 
-     $              - sum_lo*RhoYe_lo/sumRhoY_lo
+     $              - sum_gamma_lo*RhoXe_lo/sumRhoX_lo
                gamma_hi(i,n) = gamma_hi(i,n) 
-     $              - sum_hi*RhoYe_hi/sumRhoY_hi
+     $              - sum_gamma_hi*RhoXe_hi/sumRhoX_hi
 
             end do
          end if
