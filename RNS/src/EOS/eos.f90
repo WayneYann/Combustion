41e798a4650dd3771a1b25292e9a69980d07dc9e
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-06-20 12:45:26 -0700

    RNS: update EOS

--- RNS/src/EOS/eos.f90
+++ RNS/src/EOS/eos.f90
@@ -1,45 +1,36 @@
 module eos_module 
 
-  use chemistry_module, only : nspecies, Ru
+  use chemistry_module, only : nspecies, Ru, inv_mwt
 
   implicit none
 
-  double precision, save, private :: smalld
-  double precision, save, private :: smallt
-  double precision, save, private :: smallp
-  double precision, save, private :: smallc
+  double precision, save, private :: smalld = 1.d-50
+  double precision, save, private :: smallt = 1.d-50
+  double precision, save, private :: smallp = 1.d-50
+  double precision, save, private :: smallc = 1.d-50
+
+  logical, save, private :: initialized = .false.
 
 contains
 
-  subroutine eos_init(small_temp, small_dens)
-
-    implicit none
- 
+  subroutine eos_init(small_temp, small_dens, gamma_in)
     double precision, intent(in), optional :: small_temp
     double precision, intent(in), optional :: small_dens
-    
+    double precision, intent(in), optional :: gamma_in
+
     if (present(small_temp)) then
        if (small_temp > 0.d0) then
           smallt = small_temp
-       else
-          smallt = 250.d0
        end if
-    else
-       smallt = 250.d0
     endif
  
     if (present(small_dens)) then
        if (small_dens > 0.d0) then
           smalld = small_dens
-       else
-          smalld = 1.d-50
        end if
-    else
-       smalld = 1.d-50
     endif
- 
-    smallp = 1.d-50
-    smallc = 1.d-50
+
+    initialized = .true.
     
   end subroutine eos_init
 
@@ -61,17 +52,14 @@ contains
 
 
   subroutine eos_get_c(c, rho, T, Y, pt_index)
-
-    implicit none
-
     double precision, intent(out) :: c
     double precision, intent(in) :: rho, T, Y(nspecies)
     integer, optional, intent(in) :: pt_index(:)
-
     integer :: iwrk
-    double precision :: rwrk, Cv, gamma, p
+    double precision :: rwrk, Cv, gamma, p, X(nspecies)
 
-    call ckcvbs(T, Y, iwrk, rwrk, Cv)
+    call ckytx(Y, iwrk, rwrk, X)
+    call ckcvbl(T, X, iwrk, rwrk, Cv)
 
     gamma = (Cv + Ru) / Cv
 
@@ -79,126 +67,116 @@ contains
 
     c = sqrt(gamma*p/rho)
     c = max(c, smallc)
-
   end subroutine eos_get_c
 
 
   subroutine eos_get_T(T, e, Y, pt_index)
-
-    implicit none
-
     double precision, intent(out) :: T
     double precision, intent(in) :: e, Y(nspecies)
     integer, optional, intent(in) :: pt_index(:)
-
-    integer :: iwrk
+    integer :: iwrk, ierr
     double precision :: rwrk
-
-    call feeytt(e, Y, iwrk, rwrk, T)
+    call get_T_given_eY(e, Y, iwrk, rwrk, T, ierr)
+    if (ierr .ne. 0) then
+       print *, 'EOS: get_T failed, T, e, Y = ', T, e, Y
+       call flush(6)
+       stop
+    end if
     T = max(T, smallt)
-
   end subroutine eos_get_T
 
 
-  subroutine eos_get_p(p, rho, T, Y, pt_index)
+  ! subroutine eos_get_p(p, rho, T, Y, pt_index)
+  !   double precision, intent(out) :: p
+  !   double precision, intent(in) :: rho, T, Y(nspecies)
+  !   integer, optional, intent(in) :: pt_index(:)
 
-    implicit none
+  !   integer :: iwrk
+  !   double precision :: rwrk
 
-    double precision, intent(out) :: p
-    double precision, intent(in) :: rho, T, Y(nspecies)
-    integer, optional, intent(in) :: pt_index(:)
+  !   call ckpy(rho, T, Y, iwrk, rwrk, p)
+  !   p = max(p,smallp)
 
-    integer :: iwrk
-    double precision :: rwrk
-
-    call ckpy(rho, T, Y, iwrk, rwrk, p)
-    p = max(p,smallp)
-
-  end subroutine eos_get_p
+  ! end subroutine eos_get_p
 
 
-  subroutine eos_get_TP(T, P, rho, e, Y, pt_index)
+  ! subroutine eos_get_TP(T, P, rho, e, Y, pt_index)
+  !   double precision, intent(out) :: T, P
+  !   double precision, intent(in ) :: rho, e, Y(nspecies)
+  !   integer, optional, intent(in   ) :: pt_index(:)
+    
+  !   integer :: iwrk, ierr
+  !   double precision :: rwrk
 
-    implicit none
+  !   call get_T_given_eY(e, Y, iwrk, rwrk, T, ierr)
+  !   T = max(T, smallt)
 
-    double precision, intent(out) :: T, P
-    double precision, intent(in ) :: rho, e, Y(:)
+  !   call ckpy(rho, T, Y, iwrk, rwrk, p)
+  !   p = max(p,smallp)    
+
+  ! end subroutine eos_get_TP
+
+
+  ! subroutine eos_get_eP(e, P, rho, T, Y, pt_index)
+  !   double precision, intent(  out) :: e, P
+  !   double precision, intent(in   ) :: rho, T, Y(nspecies)
+  !   integer, optional, intent(in   ) :: pt_index(:)
+    
+  !   integer :: iwrk
+  !   double precision :: rwrk
+
+  !   call ckpy(rho, T, Y, iwrk, rwrk, P)
+  !   P = max(P,smallp)
+
+  !   call ckubms(T,Y,iwrk,rwrk,e)
+
+  ! end subroutine eos_get_eP
+
+
+  subroutine eos_given_RTY(e, P, C, dpdr, dpde, rho, T, Y, pt_index)
+    double precision, intent(  out) :: e, P, C, dpdr(nspecies), dpde
+    double precision, intent(in   ) :: rho, T, Y(nspecies)
     integer, optional, intent(in   ) :: pt_index(:)
     
     integer :: iwrk
-    double precision :: rwrk
-
-    call feeytt(e, Y, iwrk, rwrk, T)
-    T = max(T, smallt)
-
-    call ckpy(rho, T, Y, iwrk, rwrk, p)
-    p = max(p,smallp)    
-
-  end subroutine eos_get_TP
-
-
-  subroutine eos_get_eP(e, P, rho, T, Y, pt_index)
-    
-    implicit none
-    
-    double precision, intent(  out) :: e, P
-    double precision, intent(in   ) :: rho, T, Y(:)
-    integer, optional, intent(in   ) :: pt_index(:)
-    
-    integer :: iwrk
-    double precision :: rwrk
+    double precision :: rwrk, Cv, G, X(nspecies)
 
     call ckpy(rho, T, Y, iwrk, rwrk, P)
     P = max(P,smallp)
 
     call ckubms(T,Y,iwrk,rwrk,e)
 
-  end subroutine eos_get_eP
-
-
-  subroutine eos_given_RTY(e, P, C, G, dpdr, dpde, rho, T, Y, pt_index)
-    
-    implicit none
-    
-    double precision, intent(  out) :: e, P, C, G, dpdr, dpde
-    double precision, intent(in   ) :: rho, T, Y(:)
-    integer, optional, intent(in   ) :: pt_index(:)
-    
-    integer :: iwrk
-    double precision :: rwrk, Cv
-
-    call ckpy(rho, T, Y, iwrk, rwrk, P)
-    P = max(P,smallp)
-
-    call ckubms(T,Y,iwrk,rwrk,e)
-
-    call ckcvbs(T, Y, iwrk, rwrk, Cv)
+    call ckytx(Y, iwrk, rwrk, X)
+    call ckcvbl(T, Y, iwrk, rwrk, Cv)
     G = (Cv + Ru) / Cv
 
     C = sqrt(G*P/rho)
     C = max(C, smallc)
 
-    dpdr = P/rho
-    dpde = P/(Cv*T)
+    dpdr = Ru*T*inv_mwt
+    dpde = (G-1.d0)*rho
 
   end subroutine eos_given_RTY
 
 
-  subroutine eos_given_ReY(G, P, C, T, dpdr, dpde, rho, e, Y, pt_index)
- 
-    implicit none
-    
-    double precision, intent(  out) :: G, P, C, T, dpdr, dpde
-    double precision, intent(in   ) :: rho, e, Y(:)
-    integer, optional, intent(in   ) :: pt_index(:)
+  subroutine eos_given_ReY(P, C, T, dpdr, dpde, rho, e, Y, pt_index)
+    double precision, intent(  out) :: P, C, T, dpdr(nspecies), dpde
+    double precision, intent(in   ) :: rho, e, Y(nspecies)
+    integer, optional, intent(in  ) :: pt_index(:)
 
-    integer :: iwrk
-    double precision :: rwrk, Cv
+    integer :: n, iwrk, ierr
+    double precision :: rwrk, Cv, G, X(nspecies)
 
-    call feeytt(e, Y, iwrk, rwrk, T)
+    call get_T_given_eY(e, Y, iwrk, rwrk, T, ierr)
+    if (ierr .ne. 0) then
+       print *, 'EOS: get_T failed, T, e, Y = ', T, e, Y
+       call flush(6)
+       stop
+    end if
     T = max(T, smallt)
     
-    call ckcvbs(T, Y, iwrk, rwrk, Cv)
+    call ckytx(Y, iwrk, rwrk, X)
+    call ckcvbl(T, X, iwrk, rwrk, Cv)
     G = (Cv + Ru) / Cv
 
     call ckpy(rho, T, Y, iwrk, rwrk, P)
@@ -206,9 +184,9 @@ contains
     C = sqrt(G*P/rho)
     C = max(C, smallc)
 
-    dpdr = P/rho
-    dpde = P/(Cv*T)
-    
+    dpdr = Ru*T*inv_mwt
+    dpde = (G-1.d0)*rho
+
   end subroutine eos_given_ReY
 
 end module eos_module 
