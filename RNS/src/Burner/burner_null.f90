61a5ddbfd3f94ee3c4c44717738d87adda372ddc
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-06-24 21:10:24 -0700

    RNS: added burner for null chemistry so that the code compiles for pure hydro problems

---
+++ RNS/src/Burner/burner_null.f90
@@ -0,0 +1,19 @@
+module burner_module
+
+  implicit none
+
+  private
+
+  public :: burn
+
+contains
+
+  subroutine burn(rho, YT, dt)
+    double precision, intent(in   ) :: rho, dt
+    double precision, intent(inout) :: YT(*)
+
+    return
+
+  end subroutine burn
+
+end module burner_module
