460c879fd057fc27012bff965632b9af86b94e5b
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-09-12 15:41:19 -0700

    RNS: interface change in 2D hypterm and difterm

--- RNS/src/Src_2d/difterm.f90
+++ RNS/src/Src_2d/difterm.f90
@@ -11,12 +11,12 @@ module difterm_module
 
 contains
 
-  subroutine difterm(lo,hi,U,Ulo,Uhi,fx,fy,dxinv)
-    integer, intent(in) :: lo(2), hi(2), Ulo(2), Uhi(2)
+  subroutine difterm(lo,hi,U,Ulo,Uhi,fx,fxlo,fxhi,fy,fylo,fyhi,dxinv)
+    integer, intent(in) :: lo(2), hi(2), Ulo(2), Uhi(2), fxlo(2), fxhi(2), fylo(2), fyhi(2)
     double precision, intent(in   ) :: dxinv(2)
-    double precision, intent(in   ) ::  U(Ulo(1):Uhi(1)  ,Ulo(2):Uhi(2)  ,NVAR)
-    double precision, intent(inout) :: fx( lo(1): hi(1)+1, lo(2): hi(2)  ,NVAR)
-    double precision, intent(inout) :: fy( lo(1): hi(1)  , lo(2): hi(2)+1,NVAR)
+    double precision, intent(in   ) ::  U( Ulo(1): Uhi(1), Ulo(2): Uhi(2),NVAR)
+    double precision, intent(inout) :: fx(fxlo(1):fxhi(1),fxlo(2):fxhi(2),NVAR)
+    double precision, intent(inout) :: fy(fylo(1):fyhi(1),fylo(2):fyhi(2),NVAR)
 
     double precision, dimension(:,:,:), pointer :: Uag => Null()
     double precision, allocatable, target :: U1(:,:,:), U2(:,:,:)
