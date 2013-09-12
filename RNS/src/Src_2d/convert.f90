bfcfbec927e186ab94e7a984ecb52393757480f3
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-09-12 16:50:35 -0700

    RNS: 2D diffusion work in progress

--- RNS/src/Src_2d/convert.f90
+++ RNS/src/Src_2d/convert.f90
@@ -4,22 +4,43 @@ module convert_module
 
   private
 
-  public :: cellavg2cc_1d
+  public :: cellavg2cc
 
 contains
 
-  subroutine cellavg2cc_1d(ua, alo, ahi, uc, clo, chi)
-    integer, intent(in) :: alo, ahi, clo, chi
-    double precision, intent(in) :: ua(alo:ahi)
-    double precision, intent(out) :: uc(clo:chi)
+  subroutine cellavg2cc(lo, hi, ua, alo, ahi, uc, clo, chi)
+    integer, intent(in) :: lo(2), hi(2), alo(2), ahi(2), clo(2), chi(2)
+    double precision, intent(in) :: ua(alo(1):ahi(1),alo(2):ahi(2))
+    double precision             :: uc(clo(1):chi(1),clo(2):chi(2))
     
-    integer :: i
-    double precision, parameter :: b = -1.d0/24.d0, d = 13.d0/12.d0
+    integer :: i, j
+    double precision, parameter :: b = -1.d0/24.d0, d = 13.d0/12.d0, sevenSixth=7.d0/6.d0
 
-    do i=clo,chi
-       uc(i) = b*ua(i-1) + d*ua(i) + b*ua(i+1) 
-    end do
+    if (lo(1) .eq. hi(1)) then
 
-  end subroutine cellavg2cc_1d
+       i = lo(1)
+       do j=lo(2),hi(2)
+          uc(i,j) = b*(ua(i,j-1)+ua(i,j+1)) + d*ua(i,j) 
+       end do
+
+    else if (lo(2) .eq. hi(2)) then
+
+       j = lo(2)
+       do i=lo(1),hi(1)
+          uc(i,j) = b*(ua(i-1,j)+ua(i+1,j)) + d*ua(i,j) 
+       end do
+
+    else
+
+       do j=lo(2),hi(2)
+          do i=lo(1),hi(1)
+             uc(i,j) = b*(ua(i-1,j-1)+ua(i+1,j-1)+ua(i-1,j+1)+ua(i+1,j+1)) &
+                  + sevenSixth*ua(i,j)
+          end do
+       end do
+
+    end if
+
+  end subroutine cellavg2cc
 
 end module convert_module
--- RNS/src/Src_2d/difterm.f90
+++ RNS/src/Src_2d/difterm.f90
@@ -2,6 +2,9 @@ module difterm_module
 
   use meth_params_module, only : NVAR, NSPEC, QCVAR, QFVAR
   use weno_module, only : cellavg2gausspt_1d, cellavg2face_1d
+  use convert_module, only : cellavg2cc
+  use variables_module, only : ctoprim
+  use transport_properties, only : get_transport_properties
 
   implicit none
 
@@ -21,22 +24,36 @@ contains
     double precision, dimension(:,:,:), pointer :: Uag => Null()
     double precision, allocatable, target :: U1(:,:,:), U2(:,:,:)
     double precision, allocatable :: Qc(:,:,:), Qf(:,:,:)
-    double precision, allocatable :: mu(:), xi(:), lam(:), Ddia(:,:)
+    double precision, allocatable :: mu(:,:), xi(:,:), lam(:,:), Ddia(:,:,:)
     integer :: i, j, n, g
+    integer :: tlo(3), thi(3), Qclo(3), Qchi(3), Qflo(3), Qfhi(3), g2lo(2), g2hi(2)
+
+    g2lo = lo-2;  g2hi = hi+2
+    allocate(U1(g2lo(1):g2hi(1),g2lo(2):g2hi(2),NVAR))
+    allocate(U2(g2lo(1):g2hi(1),g2lo(2):g2hi(2),NVAR))
+
+    tlo = 1; thi = 1; Qclo = 1;  Qchi = 1;  Qflo = 1;  Qfhi = 1
+
+    Qclo(1:2) = lo(1:2)-2
+    Qchi(1:2) = hi(1:2)+2
+
+    Qflo(1:2) = lo(1:2)
+    Qfhi(1:2) = hi(1:2)+1
+
+    allocate(Qc  (Qclo(1):Qchi(1),Qclo(2):Qchi(2),QCVAR))
+    allocate(Qf  (Qflo(1):Qfhi(1),Qflo(2):Qfhi(2),QFVAR))
+    allocate(mu  (Qflo(1):Qfhi(1),Qflo(2):Qfhi(2)))
+    allocate(xi  (Qflo(1):Qfhi(1),Qflo(2):Qfhi(2)))
+    allocate(lam (Qflo(1):Qfhi(1),Qflo(2):Qfhi(2)))
+    allocate(Ddia(Qflo(1):Qfhi(1),Qflo(2):Qfhi(2),NSPEC))
 
     ! ----- compute x-direction flux first -----
-
-    allocate(U1(lo(1)-2:hi(1)+2,lo(2):hi(2),NVAR))
-    allocate(U2(lo(1)-2:hi(1)+2,lo(2):hi(2),NVAR))
-
-    allocate(Qf(lo(1)-1:hi(1)+1,lo(2):hi(2),QFVAR))
-
 
     ! cell-average => cell-avg-in-x and Gauss-point-in-y
     do n=1,NVAR
        do i=lo(1)-2,hi(1)+2
-!          call cellavg2gausspt_1d(U(i,lo(2)-2:hi(2)+2,n), lo(2)-2, hi(2)+2, &
-!               U1, U2, lo(2), hi(2))
+          call cellavg2gausspt_1d(lo(2),hi(2), U(i,:,n), Ulo(2), Uhi(2), &
+               U1(i,:,:), U2(i,:,:), g2lo(2), g2hi(2))
        end do
     end do
 
@@ -48,13 +65,35 @@ contains
           Uag => U2
        end if
 
-       ! cell-avg-in-x and Gauss-point-in-y => xface and Gauss-point-in-y
        do n=1,NVAR
           do j=lo(2),hi(2)
-!             call cellavg2face_1d(U1(:,j,n), lo(1)-2, hi(1)+2, &
-!                  Qf1, lo(1), hi(1))
+             ! cell-avg-in-x and Gauss-point-in-y => xface and Gauss-point-in-y
+             call cellavg2face_1d(lo(1),hi(1)+1, Uag(:,j,n),g2lo(1),g2hi(1), &
+                  Qf(:,j,n),Qflo(1),Qfhi(1))
+
+             ! cell-avg-in-x and Gauss-point-in-y => cell-center-in-x and Gauss-point-in-y
+             tlo(1) = lo(1)-2
+             tlo(2) = j
+             thi(1) = hi(1)+2
+             thi(2) = j
+             call cellavg2cc(tlo(1:2),thi(1:2), Uag(:,:,n),g2lo,g2hi, &
+                  Qc(:,:,n),Qclo(1:2),Qchi(1:2))
           end do
        end do
+
+       tlo(1:2) = lo(1:2)
+       thi(1) = hi(1)+1
+       thi(2) = hi(2)
+       call ctoprim(tlo,thi, Qf, Qflo,Qfhi,QFVAR)
+
+       tlo(1) = lo(1)-2
+       thi(1) = hi(1)+2
+       call ctoprim(tlo,thi, Qf, Qflo,Qfhi,QFVAR)
+
+       ! transport coefficients on face
+       call get_transport_properties(Qflo,Qfhi, Qf,Qflo,Qfhi,QFVAR, mu,xi,lam,Ddia,Qflo,Qfhi)
+
+!       call comp_diff_flux_x()
 
        Nullify(Uag)
     end do
