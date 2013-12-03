e37cb542b19ed0001953fcd1cc11c374f50083ae
Weiqun Zhang <weiqunzhang@lbl.gov>
2013-12-03 15:02:34 -0800

    RNS: new way of computing transport coefficients implemented in 2D

--- RNS/src/Src_nd/polyinterp.f90
+++ RNS/src/Src_nd/polyinterp.f90
@@ -18,21 +18,70 @@ module polyinterp_module
 
   private
 
-  public :: cc2face_1d !, cc2gaussptface_2d
+  public :: cc2xface_2d, cc2yface_2d
 
 contains
 
-  subroutine cc2face_1d(lo, hi, u, ulo, uhi, uf, flo, fhi)
-    integer, intent(in) :: lo, hi, ulo, uhi, flo, fhi
-    double precision, intent(in) ::  u(ulo:uhi)
-    double precision             :: uf(flo:fhi)
-    integer :: i
-    do i=lo,hi
-       uf(i) = -0.0625d0*(u(i-2)+u(i+1)) + 0.5626d0*(u(i-1)+u(i))
+  ! subroutine cc2face_1d(lo, hi, u, ulo, uhi, uf, flo, fhi)
+  !   integer, intent(in) :: lo, hi, ulo, uhi, flo, fhi
+  !   double precision, intent(in) ::  u(ulo:uhi)
+  !   double precision             :: uf(flo:fhi)
+  !   integer :: i
+  !   do i=lo,hi
+  !      uf(i) = -0.0625d0*(u(i-2)+u(i+1)) + 0.5626d0*(u(i-1)+u(i))
+  !   end do
+  ! end subroutine cc2face_1d
+
+  subroutine cc2xface_2d(clo,chi,u,ulo,uhi,u1,u2,flo,fhi)
+    integer, intent(in) :: clo(2),chi(2),ulo(2),uhi(2),flo(2),fhi(2)
+    double precision, intent(in ) :: u (ulo(1):uhi(1),ulo(2):uhi(2))
+    double precision              :: u1(flo(1):fhi(1),flo(2):fhi(2))
+    double precision              :: u2(flo(1):fhi(1),flo(2):fhi(2))
+
+    integer :: i, j
+    double precision :: uf(clo(1):chi(1)+1,clo(2)-2:chi(2)+2)
+
+    do  j=clo(2)-2, chi(2)+2
+     do i=clo(1)   ,chi(1)+1
+          uf(i,j) = -0.0625d0*(u(i-2,j)+u(i+1,j)) + 0.5626d0*(u(i-1,j)+u(i,j))
+       end do
     end do
-  end subroutine cc2face_1d
 
+    do    j=clo(2),chi(2)
+       do i=clo(1),chi(1)+1
+          u1(i,j) = cg1(-2)*uf(i,j-2)+cg1(-1)*uf(i,j-1)+cg1(0)*uf(i,j) &
+               +    cg1( 1)*uf(i,j+1)+cg1( 2)*uf(i,j+2)
+          u2(i,j) = cg2(-2)*uf(i,j-2)+cg2(-1)*uf(i,j-1)+cg2(0)*uf(i,j) &
+               +    cg2( 1)*uf(i,j+1)+cg2( 2)*uf(i,j+2)
+       end do
+    end do
 
-!  subroutine cc2gaussptface_2d(lo, hi, 
+  end subroutine cc2xface_2d
+
+  subroutine cc2yface_2d(clo,chi,u,ulo,uhi,u1,u2,flo,fhi)
+    integer, intent(in) :: clo(2),chi(2),ulo(2),uhi(2),flo(2),fhi(2)
+    double precision, intent(in ) :: u (ulo(1):uhi(1),ulo(2):uhi(2))
+    double precision              :: u1(flo(1):fhi(1),flo(2):fhi(2))
+    double precision              :: u2(flo(1):fhi(1),flo(2):fhi(2))
+
+    integer :: i, j
+    double precision :: uf(clo(1)-2:chi(1)+2,clo(2):chi(2)+1)
+
+    do  j=clo(2)  ,chi(2)+1
+     do i=clo(1)-2,chi(1)+2
+          uf(i,j) = -0.0625d0*(u(i,j-2)+u(i,j+1)) + 0.5626d0*(u(i,j-1)+u(i,j))
+       end do
+    end do
+
+    do    j=clo(2),chi(2)+1
+       do i=clo(1),chi(1)
+          u1(i,j) = cg1(-2)*uf(i-2,j)+cg1(-1)*uf(i-1,j)+cg1(0)*uf(i,j) &
+               +    cg1( 1)*uf(i+1,j)+cg1( 2)*uf(i+2,j)
+          u2(i,j) = cg2(-2)*uf(i-2,j)+cg2(-1)*uf(i-1,j)+cg2(0)*uf(i,j) &
+               +    cg2( 1)*uf(i+1,j)+cg2( 2)*uf(i+2,j)
+       end do
+    end do
+
+  end subroutine cc2yface_2d
 
 end module polyinterp_module
