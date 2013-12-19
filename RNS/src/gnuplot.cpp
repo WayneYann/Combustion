7e0f954f7b28295b86566a4562ee474224855d95
Matthew Emmett <mwemmett@lbl.gov>
2013-12-19 09:36:45 -0800

    RNS: gnuplot: More C, less C++ :)

--- RNS/src/gnuplot.cpp
+++ RNS/src/gnuplot.cpp
@@ -5,82 +5,76 @@
 #include <cstring>
 #include <unistd.h>
 
-#include <map>
-#include <iostream>
-#include <sstream>
-
 #include <MultiFab.H>
 
-using namespace std;
+#define GPCHKERR(err, msg) if ((err) == -1) { perror(msg); return -1; }
+#define MAXWINDOWS 12
 
-map<int,FILE*> gp, from;
-void dgp_connect(int window);
+FILE *to[MAXWINDOWS] = { 0 };
+FILE *fr[MAXWINDOWS] = { 0 };
 
+/*
+ * Fork a new gnuplot process and connect pipes.
+ */
+int dgp_connect(FILE **fto, FILE **ffr)
+{
+  int err, pid, to[2], fr[2];
+
+  err = pipe(to); GPCHKERR(err, "Unable to create pipe.");
+  err = pipe(fr); GPCHKERR(err, "Unable to create pipe.");
+  pid = fork();   GPCHKERR(pid, "Unable to fork gnuplot.");
+
+  if (pid) {
+    close(to[0]); close(fr[1]);
+  } else {
+    close(to[1]); close(fr[0]);
+    dup2(to[0], 0); close(to[0]);
+    dup2(fr[1], 1); close(fr[1]);
+    execl("/usr/bin/gnuplot", "gnuplot", NULL);
+  }
+
+  *fto = fdopen(to[1], "w");
+  *ffr = fdopen(fr[0], "r");
+
+  return 0;
+}
+
+/*
+ * Send a MultiFab to a gnuplot window.
+ */
 void dgp_send_mf(MultiFab& U, int level, int comp, int wait)
 {
-  if (gp.count(level) == 0) dgp_connect(level);
+  if (level >= MAXWINDOWS) return;
+
+  if (to[level] == 0) {
+    dgp_connect(&to[level], &fr[level]);
+  }
 
   const Box& mbx = U.boxArray().minimalBox();
-  fprintf(gp[level], "set term wxt %d\n", level);
-  fprintf(gp[level], "set dgrid3d %d, %d\n", mbx.length(0)+2*U.nGrow(), mbx.length(1)+2*U.nGrow());
-  fprintf(gp[level], "set pm3d\n");
-  fprintf(gp[level], "set hidden3d\n");
-  fprintf(gp[level], "splot '-'\n");
+  fprintf(to[level], "set term wxt %d\n", level);
+  fprintf(to[level], "set dgrid3d %d, %d\n", mbx.length(0)+2*U.nGrow(), mbx.length(1)+2*U.nGrow());
+  fprintf(to[level], "set pm3d\n");
+  fprintf(to[level], "set hidden3d\n");
+  fprintf(to[level], "splot '-'\n");
   for (MFIter mfi(U); mfi.isValid(); ++mfi) {
     const Box& bx = U[mfi].box();
     IntVect    sm = bx.smallEnd();
     IntVect    bg = bx.bigEnd();
     for (IntVect p=sm; p<=bg; bx.next(p))
-      fprintf(gp[level], "%d %d %e\n", p[0], p[1], U[mfi](p,comp));
+      fprintf(to[level], "%d %d %e\n", p[0], p[1], U[mfi](p,comp));
   }
-  fprintf(gp[level], "e\n");
+  fprintf(to[level], "e\n");
   if (wait) {
-    fprintf(gp[level], "set print\n");
-    fprintf(gp[level], "pause mouse button3 \"===> paused - button 3 in window %d to release\"\n", level);
-    fprintf(gp[level], "print ''\n");
-    fprintf(gp[level], "set print '-'\n");
-    fprintf(gp[level], "print 'hoser!'\n");
+    fprintf(to[level], "set print\n");
+    fprintf(to[level], "pause mouse button3 \"===> paused - button 3 in window %d to release\"\n", level);
+    fprintf(to[level], "print ''\n");
+    fprintf(to[level], "set print '-'\n");
+    fprintf(to[level], "print 'hoser!'\n");
   }
-  fflush(gp[level]);
+  fflush(to[level]);
 
   if (wait) {
     char buf[8];
-    char *s = fgets(buf, 8, from[level]);
+    char *s = fgets(buf, 8, fr[level]);
   }
-}
-
-void dgp_connect(int window)
-{
-  int err, pid, fdto[2], fdfrom[2];
-
-  err = pipe(fdto);
-  if (err == -1) {
-    perror("Unable to create pipe");
-    return;
-  }
-
-  err = pipe(fdfrom);
-  if (err == -1) {
-    perror("Unable to create pipe");
-    return;
-  }
-
-  pid = fork();
-  if (pid == -1) {
-    perror("Unable to fork gnuplot");
-    return;
-  }
-
-  if (pid) {
-    close(fdto[0]);
-    close(fdfrom[1]);
-  } else {
-    close(fdto[1]); close(fdfrom[0]);
-    dup2(fdto[0], 0); close(fdto[0]);
-    dup2(fdfrom[1], 1); close(fdfrom[1]);
-    execl("/usr/bin/gnuplot", "gnuplot", NULL);
-  }
-
-  gp[window] = fdopen(fdto[1], "w");
-  from[window] = fdopen(fdfrom[0], "r");
 }
