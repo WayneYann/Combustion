7c71048d401fb6ef26b7b9a3758dd7389ae9cb2a
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-03-11 11:36:30 -0700

    RNS: added 2d conversion in 3d convert module

--- RNS/src/Src_3d/convert.f90
+++ RNS/src/Src_3d/convert.f90
@@ -4,7 +4,7 @@ module convert_module
 
   private
 
-  public :: cellavg2cc_3d, cc2cellavg_3d!, cellavg2cc_2d, cc2cellavg_2d
+  public :: cellavg2cc_3d, cc2cellavg_3d, cellavg2cc_2d, cc2cellavg_2d
 
 contains
 
@@ -45,5 +45,87 @@ contains
        end do
     end do
   end subroutine cc2cellavg_3d
+
+  subroutine cellavg2cc_2d(lo, hi, ua, alo, ahi, uc, clo, chi, face)
+    integer, intent(in) :: lo(3), hi(3), alo(3), ahi(3), clo(3), chi(3)
+    double precision, intent(in) :: ua(alo(1):ahi(1),alo(2):ahi(2),alo(3):ahi(3))
+    double precision             :: uc(clo(1):chi(1),clo(2):chi(2),clo(3):chi(3))
+    integer, intent(in) :: face
+
+    integer :: i, j, k
+    double precision, parameter :: b = -1.d0/24.d0, sevenSixth=7.d0/6.d0
+
+    if (face .eq. 1) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                uc(i,j,k) = b*(ua(i,j,k-1)+ua(i,j-1,k)+ua(i,j+1,k)+ua(i,j,k+1)) &
+                     + sevenSixth*ua(i,j,k)
+             end do
+          end do
+       end do
+    else if (face .eq. 2) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                uc(i,j,k) = b*(ua(i,j,k-1)+ua(i-1,j,k)+ua(i+1,j,k)+ua(i,j,k+1)) &
+                     + sevenSixth*ua(i,j,k)
+             end do
+          end do
+       end do
+    else if (face .eq. 3) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                uc(i,j,k) = b*(ua(i,j-1,k)+ua(i-1,j,k)+ua(i+1,j,k)+ua(i,j+1,k)) &
+                     + sevenSixth*ua(i,j,k)
+             end do
+          end do
+       end do
+    else
+       call bl_error("Error: cellavg2cc_2d: unknown face")
+    end if
+  end subroutine cellavg2cc_2d
+     
+  subroutine cc2cellavg_2d(lo, hi, uc, clo, chi, ua, alo, ahi, face)
+    integer, intent(in) :: lo(3), hi(3), alo(3), ahi(3), clo(3), chi(3)
+    double precision, intent(in) :: uc(clo(1):chi(1),clo(2):chi(2),clo(3):chi(3))
+    double precision             :: ua(alo(1):ahi(1),alo(2):ahi(2),alo(3):ahi(3))
+    integer, intent(in) :: face
+
+    integer :: i, j, k
+    double precision, parameter :: b = 1.d0/24.d0, fiveSixth=5.d0/6.d0
+
+    if (face .eq. 1) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                ua(i,j,k) = b*(uc(i,j,k-1)+uc(i,j-1,k)+uc(i,j+1,k)+uc(i,j,k+1)) &
+                     + fiveSixth*uc(i,j,k)
+             end do
+          end do
+       end do
+    else if (face .eq. 2) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                ua(i,j,k) = b*(uc(i,j,k-1)+uc(i-1,j,k)+uc(i+1,j,k)+uc(i,j,k+1)) &
+                     + fiveSixth*uc(i,j,k)
+             end do
+          end do
+       end do
+    else if (face .eq. 3) then
+       do k=lo(3),hi(3)
+          do j=lo(2),hi(2)
+             do i=lo(1),hi(1)
+                ua(i,j,k) = b*(uc(i,j-1,k)+uc(i-1,j,k)+uc(i+1,j,k)+uc(i,j+1,k)) &
+                     + fiveSixth*uc(i,j,k)
+             end do
+          end do
+       end do
+    else
+       call bl_error("Error: cc2cellavg_2d: unknown face")
+    end if
+  end subroutine cc2cellavg_2d
 
 end module convert_module
