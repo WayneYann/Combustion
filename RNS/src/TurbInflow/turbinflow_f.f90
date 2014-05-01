a658a3dadaddf7f969c2c8e7d74bb707c16f3577
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-05-01 14:19:19 -0700

    RNS: clean up turbinflow module

--- RNS/src/TurbInflow/turbinflow_f.f90
+++ RNS/src/TurbInflow/turbinflow_f.f90
@@ -4,21 +4,18 @@ module turbinflow_module
 
   logical, save :: turbinflow_initialized = .false.
 
-  character (len=128), save :: turbfile = ""  
   integer, save :: lenfname
   integer, save :: iturbfile(128)
 
-  integer, save :: ncells(3)
-  double precision, save :: probsize(3), problo(3), probhi(3)
   integer, save :: npboxcells(3)
-  double precision, save :: pboxsize(3), pboxlo(3) 
-  double precision, save :: dx(3), dxinv(3)
+  double precision, save :: pboxlo(3), dx(3), dxinv(3)
 
   integer, parameter :: nplane = 8
-  double precision, allocatable, save :: sdata(:,:,:,:), planedata(:,:,:)
+  double precision, allocatable, save :: sdata(:,:,:,:)
   double precision, save :: szlo=0.d0, szhi=0.d0
 
   integer, parameter :: isswirltype = 0  ! periodic
+  double precision, save :: units_conversion = 100.d0  ! m --> cm & m/s --> cm/s
 
   private
 
@@ -26,14 +23,23 @@ module turbinflow_module
 
 contains
 
-  subroutine init_turbinflow(turbfile_in)
-    character (len=128), intent(in) :: turbfile_in
+  subroutine init_turbinflow(turbfile, is_cgs)
+    character (len=*), intent(in) :: turbfile
+    logical, intent(in), optional :: is_cgs
 
     integer, parameter :: iunit = 20
-    integer :: ierr, n
+    integer :: ierr, n, npts(3)
+    double precision :: probsize(3), pboxsize(3)
 
-    turbfile = turbfile_in
-    lenfname = len(trim(turbfile))
+    if (present(is_cgs)) then
+       if (is_cgs) then
+          units_conversion = 100.d0
+       else
+          units_conversion = 1.d0
+       end if
+    end if
+
+    lenfname = len_trim(turbfile)
     do n=1,lenfname
        iturbfile(n) = ichar(turbfile(n:n))
     end do
@@ -46,21 +52,21 @@ contains
        call bl_abort('Problem opening file: ' // trim(turbfile) // '/HDR')
     end if
 
-    read(iunit,*) ncells
+    read(iunit,*) npts
     read(iunit,*) probsize
 
     close(iunit)
 
-    probsize = probsize * 100.d0 ! m --> cm
+    probsize = probsize * units_conversion
 
-    dx = probsize / dble(ncells-1)
+    dx = probsize / dble(npts-1)
     dxinv = 1.d0/dx
 
     pboxsize(1:2) = probsize(1:2) - 2.d0*dx(1:2)  ! because there is one ghost point on each side
     pboxsize(3) = probsize(3) ! no ghost point in z-direction
 
-    npboxcells(1:2) = ncells(1:2) - 3
-    npboxcells(3)   = ncells(3) - 1
+    npboxcells(1:2) = npts(1:2) - 3
+    npboxcells(3)   = npts(3) - 1
     
     ! period box covers -0.5*pboxsize(1) <= x <= 0.5*pboxsize(1)
     !                   -0.5*pboxsize(2) <= y <= 0.5*pboxsize(2)
@@ -68,13 +74,7 @@ contains
     pboxlo(1:2) = -0.5d0*pboxsize(1)
     pboxlo(3) = 0.d0
 
-    problo(1:2) = -0.5d0*probsize(1:2)
-    problo(3)   =  0.d0
-    probhi(1:2) =  0.5d0*probsize(1:2)
-    probhi(3)   =  probsize(3)
-
-    allocate(sdata(ncells(1),ncells(2),nplane,3))
-    allocate(planedata(ncells(1),ncells(2),3))
+    allocate(sdata(npts(1),npts(2),nplane,3))
 
     turbinflow_initialized = .true.
 
@@ -90,6 +90,8 @@ contains
     integer :: i, j, k, n, i0, j0, k0, ii, jj
     double precision :: xx, yy, zz, zdata(0:2,0:2), ydata(0:2)
     double precision :: cx(0:2), cy(0:2), cz(0:2) 
+
+    if (.not. turbinflow_initialized) call bl_error("turbinflow module uninitialized")
 
     if (z.lt.szlo+0.5d0*dx(3) .or. z.gt.szhi-0.5d0*dx(3)) then
        call store_planes(z)
@@ -151,7 +153,7 @@ contains
        do iplane=1,nplane
           k = modulo(izlo+iplane-1, npboxcells(3)) + 1
           call getplane(iturbfile, lenfname, sdata(:,:,iplane,n), k, n, isswirltype)
-          sdata(:,:,iplane,n) = sdata(:,:,iplane,n)*100.d0  ! m/s --> cm/s
+          sdata(:,:,iplane,n) = sdata(:,:,iplane,n)*units_conversion
        end do
     end do
   end subroutine store_planes
