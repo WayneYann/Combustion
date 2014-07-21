3139ab1f8ae27862f0f6f9e727c61208e61bb70c
Weiqun Zhang <weiqunzhang@lbl.gov>
2014-07-21 12:48:18 -0700

    RNS: 4th-order artificial viscosity

--- RNS/src/Src_2d/hypterm.f90
+++ RNS/src/Src_2d/hypterm.f90
@@ -177,7 +177,8 @@ contains
 
   subroutine add_artifical_viscocity(lo,hi,U,Ulo,Uhi,fx,fxlo,fxhi,fy,fylo,fyhi,dx)
 
-    use meth_params_module, only : NVAR, URHO, UMX, UMY, UTEMP, difmag
+    use meth_params_module, only : NVAR, URHO, UMX, UMY, UTEMP, UEDEN, UFS, NSPEC, difmag
+    use eos_module, only : eos_get_T, eos_get_c
 
     integer, intent(in) :: lo(2), hi(2), Ulo(2), Uhi(2), fxlo(2), fxhi(2), fylo(2), fyhi(2)
     double precision, intent(in   ) :: dx(2)
@@ -185,57 +186,73 @@ contains
     double precision, intent(inout) :: fx(fxlo(1):fxhi(1),fxlo(2):fxhi(2),NVAR)
     double precision, intent(inout) :: fy(fylo(1):fyhi(1),fylo(2):fyhi(2),NVAR)
     
-    double precision, allocatable :: divv(:,:), vx(:,:), vy(:,:)
-    double precision :: rhoInv, dvdx, dvdy, div1, dxinv(2)
+    double precision, allocatable :: vx(:,:), vy(:,:), cs(:,:)
+    double precision :: rhoInv, hdvdx, hdvdy, hdivv, dxinv(2), e, T, Y(nspec)
+    double precision :: nu, fac, cmin
     integer :: i, j, n
 
     dxinv = 1.d0/dx
     
-    allocate(vx  (lo(1)-1:hi(1)+1,lo(2)-1:hi(2)+1))
-    allocate(vy  (lo(1)-1:hi(1)+1,lo(2)-1:hi(2)+1))
-    allocate(divv(lo(1)  :hi(1)+1,lo(2)  :hi(2)+1))
+    allocate(vx(lo(1)-1:hi(1)+1,lo(2)-1:hi(2)+1))
+    allocate(vy(lo(1)-1:hi(1)+1,lo(2)-1:hi(2)+1))
+    allocate(cs(lo(1)-1:hi(1)+1,lo(2)-1:hi(2)+1))
     
     do    j=lo(2)-1,hi(2)+1
        do i=lo(1)-1,hi(1)+1
           rhoInv = 1.d0/U(i,j,URHO)
           vx(i,j) = U(i,j,UMX)*rhoInv
           vy(i,j) = U(i,j,UMY)*rhoInv
+          e  = U(i,j,UEDEN)*rhoInv - 0.5d0*(vx(i,j)**2+vy(i,j)**2)
+          Y = U(i,j,UFS:UFS+NSPEC-1)*rhoInv
+          T = U(i,j,UTEMP)
+          call eos_get_T(T, e, Y)
+          call eos_get_c(cs(i,j), U(i,j,URHO), T, Y)
        end do
     end do
+
+    ! x-direction
+
+    fac = 0.25d0*dx(1)*dxinv(2)
     
-    do    j=lo(2),hi(2)+1
+    do j=lo(2),hi(2)
        do i=lo(1),hi(1)+1
-          
-          dvdx = 0.5d0*(vx(i,j)-vx(i-1,j)+vx(i,j-1)-vx(i-1,j-1))*dxinv(1)
-          dvdy = 0.5d0*(vy(i,j)-vy(i,j-1)+vy(i-1,j)-vy(i-1,j-1))*dxinv(2)
-          divv(i,j) = dvdx + dvdy
-          
+          hdvdx = vx(i,j)-vx(i-1,j)
+          hdvdy = fac*(-vy(i-1,j-1)-vy(i,j-1)+vy(i-1,j+1)+vy(i,j+1))
+          hdivv = hdvdx + hdvdy
+          if (hdivv .lt. 0.d0) then
+             cmin = min(cs(i-1,j),cs(i,j))
+             nu = difmag * hdivv * min(hdivv*hdivv/(cmin*cmin*difmag), 1.d0)
+             do n=1,NVAR
+                if (n.ne.UTEMP) then
+                   fx(i,j,n) = fx(i,j,n) - nu*(U(i-1,j,n)-U(i,j,n))
+                end if
+             end do
+          end if
        end do
     end do
+
+    ! y-direction
+
+    fac = 0.25d0*dxinv(1)*dx(2)
     
-    do n=1,NVAR
-       if (n.ne.UTEMP) then
-          
-          do   j = lo(2),hi(2)
-             do i = lo(1),hi(1)+1
-                div1 = .5d0*(divv(i,j) + divv(i,j+1))
-                div1 = difmag*min(0.d0,div1)
-                fx(i,j,n) = fx(i,j,n) + dx(1)*div1*(U(i,j,n) - U(i-1,j,n))
-             enddo
-          enddo
-          
-          do    j = lo(2),hi(2)+1
-             do i = lo(1),hi(1)
-                div1 = .5d0*(divv(i,j) + divv(i+1,j))
-                div1 = difmag*min(0.d0,div1)
-                fy(i,j,n) = fy(i,j,n) + dx(2)*div1*(U(i,j,n) - U(i,j-1,n))
-             enddo
-          enddo
-          
-       end if
+    do j=lo(2),hi(2)+1
+       do i=lo(1),hi(1)
+          hdvdx = fac*(-vx(i-1,j-1)-vx(i-1,j)+vx(i+1,j-1)+vx(i+1,j))
+          hdvdy = vy(i,j)-vy(i,j-1)
+          hdivv = hdvdx + hdvdy
+          if (hdivv .lt. 0.d0) then
+             cmin = min(cs(i,j-1),cs(i,j))
+             nu = difmag * hdivv * min(hdivv*hdivv/(cmin*cmin*difmag), 1.d0)
+             do n=1,NVAR
+                if (n.ne.UTEMP) then
+                   fy(i,j,n) = fy(i,j,n) - nu*(U(i,j-1,n)-U(i,j,n))
+                end if
+             end do
+          end if
+       end do
     end do
-    
-    deallocate(vx,vy,divv)
+
+    deallocate(vx,vy,cs)
 
   end subroutine add_artifical_viscocity
 
