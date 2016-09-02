c721cbb259b72c5a9e0eebdf87be4cccdf67964e
Weiqun Zhang <weiqunzhang@lbl.gov>
2016-09-02 16:47:57 -0700

    move HeatTransfer::compute_rhohmix into HeatTransfer_shared.cpp because they are identical for the sdc and strang versions.

--- LMC/src/HeatTransfer.cpp
+++ LMC/src/HeatTransfer.cpp
@@ -7686,50 +7686,6 @@ HeatTransfer::compute_cp (const FArrayBox& temp,
 
     getChemSolve().getCpmixGivenTY(cp,state,state,box,sCompT,sCompY,sCompCp);
 }
-
-void
-HeatTransfer::compute_rhohmix (Real      time,
-                               MultiFab& rhohmix)
-{
-    BL_PROFILE("HeatTransfer::compute_rhohmix()");
-
-    const int ngrow  = 0; // We only do this on the valid region
-    const int sComp  = std::min(std::min((int)Density,(int)Temp),first_spec);
-    const int eComp  = std::max(std::max((int)Density,(int)Temp),first_spec+nspecies-1);
-    const int nComp  = eComp - sComp + 1;
-    const int sCompR = Density - sComp;
-    const int sCompT = Temp - sComp;
-    const int sCompY = first_spec - sComp;
-    const int sCompH = 0;
-
-    FArrayBox tmp;
-
-    for (FillPatchIterator fpi(*this,rhohmix,ngrow,time,State_Type,sComp,nComp);
-         fpi.isValid();
-         ++fpi)
-    {
-        //
-        // Convert rho*Y to Y for this operation
-        //
-	const int  iGrid    = fpi.index();
-	const Box& validBox = grids[iGrid];
-	FArrayBox& state    = fpi();
-
-        tmp.resize(validBox,1);
-        tmp.copy(state,sCompR,0,1);
-        tmp.invert(1);
-
-        for (int k = 0; k < nspecies; k++)
-            state.mult(tmp,0,sCompY+k,1);
-	
-	getChemSolve().getHmixGivenTY(rhohmix[fpi],state,state,validBox,
-				      sCompT,sCompY,sCompH);
-        //
-        // Convert hmix to rho*hmix
-        //
-        rhohmix[fpi].mult(state,validBox,sCompR,sCompH,1);
-    }
-}
  
 void
 HeatTransfer::compute_h (Real      time,
--- LMC/src/HeatTransfer_shared.cpp
+++ LMC/src/HeatTransfer_shared.cpp
@@ -38,3 +38,57 @@ HeatTransfer::timestamp_add_extras (int lev,
 
 #endif /*PARTICLES*/
 
+void
+HeatTransfer::compute_rhohmix (Real      time,
+                               MultiFab& rhohmix)
+{
+    const Real strt_time = ParallelDescriptor::second();
+
+    const int ngrow  = 0; // We only do this on the valid region
+    const int sComp  = std::min(std::min((int)Density,(int)Temp),first_spec);
+    const int eComp  = std::max(std::max((int)Density,(int)Temp),first_spec+nspecies-1);
+    const int nComp  = eComp - sComp + 1;
+    const int sCompR = Density - sComp;
+    const int sCompT = Temp - sComp;
+    const int sCompY = first_spec - sComp;
+    const int sCompH = 0;
+
+    FArrayBox tmp;
+
+    for (FillPatchIterator fpi(*this,rhohmix,ngrow,time,State_Type,sComp,nComp);
+         fpi.isValid();
+         ++fpi)
+    {
+        //
+        // Convert rho*Y to Y for this operation
+        //
+	const int  iGrid    = fpi.index();
+	const Box& validBox = grids[iGrid];
+	FArrayBox& state    = fpi();
+
+        tmp.resize(validBox,1);
+        tmp.copy(state,sCompR,0,1);
+        tmp.invert(1);
+
+        for (int k = 0; k < nspecies; k++)
+            state.mult(tmp,0,sCompY+k,1);
+	
+	getChemSolve().getHmixGivenTY(rhohmix[fpi],state,state,validBox,
+				      sCompT,sCompY,sCompH);
+        //
+        // Convert hmix to rho*hmix
+        //
+        rhohmix[fpi].mult(state,validBox,sCompR,sCompH,1);
+    }
+
+    if (verbose)
+    {
+        const int IOProc   = ParallelDescriptor::IOProcessorNumber();
+        Real      run_time = ParallelDescriptor::second() - strt_time;
+
+        ParallelDescriptor::ReduceRealMax(run_time,IOProc);
+
+        if (ParallelDescriptor::IOProcessor())
+            std::cout << "HeatTransfer::compute_rhohmix(): lev: " << level << ", time: " << run_time << '\n';
+    }
+}
